2f3e6f8b04f5de5efacebbc2a4b2f430865cdca9
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-18 12:31:00 +0200

    [file]{basic.blade.php} => remove header codes !!!

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -1,16 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-@section('metaTags')
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-@show
-@yield('jsHeader')
-@yield('css')
-    <title>@yield('title')</title>
-</head>
 <body>
 @yield('main')
 @yield('jsFooter')
