7010ed85a91ebf3bee9c08c2b900a232b64c290c
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-18 12:32:11 +0200

    [file]{header.blade.php} => make header codes !!!

--- resources/views/components/layouts/basic/header.blade.php
+++ resources/views/components/layouts/basic/header.blade.php
@@ -1,3 +1,13 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
-</div>
+<!doctype html>
+<html lang="en">
+<head>
+    @section('metaTags')
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @show
+    @yield('jsHeader')
+    @yield('css')
+    <title>@yield('title')</title>
+</head>
