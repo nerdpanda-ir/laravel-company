d060ac367fbdbdc864db00cd74b7c1088258735f
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-18 11:23:50 +0200

    [file]{basic.blade.php} => make css yield !!!

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -7,8 +7,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 @show
-    @yield('jsHeader')
-<title>Document</title>
+@yield('jsHeader')
+@yield('css')
+    <title>Document</title>
 </head>
 <body>
 
