ea509e26f99ce70a4968b3a36e955fa458f79028
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-24 07:41:43 +0200

    [file]{meta-tags.blade.php}=> create codes in meta tag component

--- resources/views/components/layouts/front/header-partials/meta-tags.blade.php
+++ resources/views/components/layouts/front/header-partials/meta-tags.blade.php
@@ -1,1 +1,14 @@
+@section('metaTags')
+    @parent
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+    <meta name='subject' content='your website'>
+    <meta name='copyright' content='company name'>
+    <meta name='robots' content='index,follow'>
+    <meta name='author' content='name, email@hotmail.com'>
+    <meta name='designer' content=''>
+    <meta name='reply-to' content='email@hotmail.com'>
+    <meta name='owner' content=''>
+    <meta name='revisit-after' content='7 days'>
+@endsection
 
