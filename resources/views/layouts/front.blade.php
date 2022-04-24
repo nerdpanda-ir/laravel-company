f383767944bf12c7506bfc7aa756042f6bda76b8
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-24 08:22:18 +0200

    [file]{front.blade.php}=> make html attributes section !!!

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -2,3 +2,4 @@
 <x-layouts.front.header-partials.meta-tags />
 <x-layouts.front.header-partials.styles />
 @section('title') page title !!! @endsection
+@section('bodyAttributes')data-spy="scroll" data-target="#navbar-example" @endsection
