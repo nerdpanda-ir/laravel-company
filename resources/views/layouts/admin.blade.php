099a4cbb01cb3b27a89b9d3e0079e399ab0b926d
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-25 12:08:17 +0200

    [file]{admin.blade.php}=> create body atttributes section !!!

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,3 +4,5 @@
 <x-layouts.admin.header-partials.meta-tags />
 <x-layouts.admin.header-partials.styles />
 
+@section('bodyAttributes')id="kt_body" style="background-image: url()" class="dark-mode header-fixed header-tablet-and-mobile-fixed aside-fixed aside-secondary-enabled"@endsection
+
