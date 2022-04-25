065e72d8ab300967727b46cc31ecb061257ad1d0
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-25 11:59:47 +0200

    [file]{styles.blade.php}=> create links tags for load admin layout styles !!!

--- resources/views/components/layouts/admin/header-partials/styles.blade.php
+++ resources/views/components/layouts/admin/header-partials/styles.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
-</div>
+@section('css')
+    <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
+    <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+    <!--begin::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700" />
+    <!--end::Fonts-->
+    <!--begin::Page Vendor Stylesheets(used by this page)-->
+    <link href="assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css" />
+    <!--end::Page Vendor Stylesheets-->
+    <!--begin::Page Vendor Stylesheets(used by this page)-->
+    <link href="assets/plugins/custom/prismjs/prismjs.bundle.rtl.css" rel="stylesheet" type="text/css" />
+    <!--end::Page Vendor Stylesheets-->
+    <!--begin::Global Stylesheets Bundle(used by all pages)-->
+    <link href="assets/plugins/global/plugins.dark.bundle.rtl.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/style.dark.bundle.rtl.css" rel="stylesheet" type="text/css" />
+    <!--end::Global Stylesheets Bundle-->
+@endsection
