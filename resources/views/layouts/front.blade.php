8fd0e381421f63fb2b965a985fa736c9f0987ef3
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-25 10:03:52 +0200

    [file]{front.blade.php}=> remove title section  !!!

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.basic')
 <x-layouts.front.header-partials.meta-tags />
 <x-layouts.front.header-partials.styles />
-@section('title') page title !!! @endsection
 @section('bodyAttributes')data-spy="scroll" data-target="#navbar-example" @endsection
 <x-layouts.front.java-scripts />
