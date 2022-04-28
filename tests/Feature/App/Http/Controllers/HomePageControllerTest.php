b1b9c297363b78a3215fc7902c4ad3d42ee4494c
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-28 13:39:58 +0200

    [file]{HomePageControllerTest.php} => create one test method

--- tests/Feature/App/Http/Controllers/HomePageControllerTest.php
+++ tests/Feature/App/Http/Controllers/HomePageControllerTest.php
@@ -10,4 +10,9 @@ use Illuminate\Contracts\View\View;
 class HomePageControllerTest extends SingleActionControllerTestCase
 {
     protected string $namespace = HomeController::class ;
+    public function test_invoke_method_should_return_view(){
+        $controller = $this->app->make($this->namespace());
+        $invokeResult = $this->app->call([$controller,'__invoke']);
+        $this->assertInstanceOf( View::class ,  $invokeResult );
+    }
 }
