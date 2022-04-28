eaf68d6d2f218a2034f24ece4453eb4977cd85e2
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-28 13:41:19 +0200

    [file]{HomeController.php}=> use View Namespace

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
 
 class HomeController extends Controller
 {
     public function __invoke() {
-        
+
     }
 }
