aaac599d43f7a81bb286be67c6681089dc6fd92d
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-28 12:03:56 +0200

    [file]{HomePageControllerTest.php} => clean test class

--- tests/Feature/App/Http/Controllers/HomePageControllerTest.php
+++ tests/Feature/App/Http/Controllers/HomePageControllerTest.php
@@ -8,15 +8,5 @@ use Tests\ControllerTestCase;
 
 class HomePageControllerTest extends ControllerTestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
+    
 }
