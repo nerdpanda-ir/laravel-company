07edc2de14fc8fa805a0f780d14cba38234e2226
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-28 12:49:52 +0200

    [file]{SingleActionControllerTestCase.php} => create method for check controller have __invoke method

--- tests/SingleActionControllerTestCase.php
+++ tests/SingleActionControllerTestCase.php
@@ -2,5 +2,11 @@
 <?php
 use Tests\ControllerTestCase ;
 class SingleActionControllerTestCase extends ControllerTestCase {
-    
+    public function test_controller_has_invoke_method():void {
+        $hasInvoke = method_exists($this->namespace(),'__invoke');
+        $this->assertTrue(
+            $hasInvoke ,
+            "every single action controllers should have __invoke method \n but {$this->namespace} class dont have __invoke method !!!! "
+        );
+    }
 }
