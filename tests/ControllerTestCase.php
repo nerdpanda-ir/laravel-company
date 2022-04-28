ce0261051bad6b2722b23151c37a603aaf045056
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-28 11:53:19 +0200

    [file]{ControllerTestCase.php} => create test_controller_class_is_exist_ method

--- tests/ControllerTestCase.php
+++ tests/ControllerTestCase.php
@@ -5,5 +5,11 @@ abstract class ControllerTestCase extends TestCase {
     protected function namespace() :string {
         return $this->namespace;
     }
-
+    public function test_controller_class_is_exist():void {
+        $isExist = class_exists($this->namespace,true);
+        $this->assertTrue(
+            $isExist,
+            'we not have this class -> '.$this->namespace()
+        );
+    }
 }
