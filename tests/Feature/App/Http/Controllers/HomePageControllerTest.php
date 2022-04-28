293e9b70001eb32bafafdbea2bac65ecc3840f92
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-28 12:05:24 +0200

    [file]{HomePageControllerTest.php} => use homeController namespace

--- tests/Feature/App/Http/Controllers/HomePageControllerTest.php
+++ tests/Feature/App/Http/Controllers/HomePageControllerTest.php
@@ -5,8 +5,9 @@ namespace Tests\Feature\App\Http\Controllers;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\ControllerTestCase;
+use App\Http\Controllers\HomeController ;
 
 class HomePageControllerTest extends ControllerTestCase
 {
-    
+
 }
