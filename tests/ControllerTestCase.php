f11d4838f3677ce03c3745371a20d13ba0d40f3a
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-28 12:51:32 +0200

    [file]{ControllerTestCase.php} => remove abstract keyword for class

--- tests/ControllerTestCase.php
+++ tests/ControllerTestCase.php
@@ -1,6 +1,6 @@
 <?php namespace Tests ; ?>
 <?php
-abstract class ControllerTestCase extends TestCase {
+class ControllerTestCase extends TestCase {
     protected string $namespace;
     protected function namespace() :string {
         return $this->namespace;
