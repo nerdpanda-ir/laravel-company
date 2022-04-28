b1ccdd22f32b0d432425680da761b30251302698
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-28 13:36:19 +0200

    [file]{HomePageControllerTest.php} => Use View Interface

--- tests/Feature/App/Http/Controllers/HomePageControllerTest.php
+++ tests/Feature/App/Http/Controllers/HomePageControllerTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\SingleActionControllerTestCase;
 use App\Http\Controllers\HomeController ;
-
+use Illuminate\Contracts\View\View;
 class HomePageControllerTest extends SingleActionControllerTestCase
 {
     protected string $namespace = HomeController::class ;
