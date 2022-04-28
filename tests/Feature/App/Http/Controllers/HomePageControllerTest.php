0d8075bfc088be4415e13a0ecbe51006a7371de0
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-28 13:00:04 +0200

    [file]{HomePageControllerTest.php} => remove ControllerTestCase namespace

--- tests/Feature/App/Http/Controllers/HomePageControllerTest.php
+++ tests/Feature/App/Http/Controllers/HomePageControllerTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature\App\Http\Controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\ControllerTestCase;
 use App\Http\Controllers\HomeController ;
 
 class HomePageControllerTest extends ControllerTestCase
