0e074f458f7954c6e0a0dbcbcb6271cff906bbf0
NerdPanda <nerdpandabusiness@gmail.com>
2022-04-30 11:17:09 +0200

    [file]{HomePageTest.php} => use TestResponse namespace

--- tests/Feature/Pages/HomePageTest.php
+++ tests/Feature/Pages/HomePageTest.php
@@ -5,10 +5,11 @@ namespace Tests\Feature\Pages;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Testing\TestResponse;
 
 class HomePageTest extends TestCase
 {
     public function test_home_page_should_have_200_status_code() :void {
-        
+
     }
 }
