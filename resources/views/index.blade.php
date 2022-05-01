2b58f31afbd09faad1bf1565452580e7ee729578
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-01 15:26:06 +0200

    [file]{index.blade.php}=> delete working with us  codes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,23 +4,6 @@
     <x-preloader />
 
 
-    <!-- Start reviews Area -->
-    <div class="reviews-area hidden-xs">
-        <div class="work-us">
-            <div class="work-left-text">
-                <a href="#">
-                    <img src="img/about/2.jpg" alt="">
-                </a>
-            </div>
-            <div class="work-right-text text-center">
-                <h2>working with us</h2>
-                <h5>Web Design, Ready Home, Construction and Co-operate Outstanding Buildings.</h5>
-                <a href="#contact" class="ready-btn">Contact us</a>
-            </div>
-        </div>
-    </div>
-    <!-- End reviews Area -->
-
     <!-- Start portfolio Area -->
     <div id="portfolio" class="portfolio-area area-padding fix">
         <div class="container">
