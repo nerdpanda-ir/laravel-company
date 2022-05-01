85e45931b05bc34c52548cfa29afcb024765a766
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-01 15:26:27 +0200

    [file]{working-with-area.blade.php}=> create working-with-us codes

--- resources/views/components/pages/home/working-with-us-area.blade.php
+++ resources/views/components/pages/home/working-with-us-area.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
+<!-- Start reviews Area -->
+<div class="reviews-area hidden-xs">
+    <div class="work-us">
+        <div class="work-left-text">
+            <a href="#">
+                <img src="img/about/2.jpg" alt="">
+            </a>
+        </div>
+        <div class="work-right-text text-center">
+            <h2>working with us</h2>
+            <h5>Web Design, Ready Home, Construction and Co-operate Outstanding Buildings.</h5>
+            <a href="#contact" class="ready-btn">Contact us</a>
+        </div>
+    </div>
 </div>
+<!-- End reviews Area -->
