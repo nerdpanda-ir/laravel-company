542f3658656c22441bec0b6eb39624f3017733c7
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-03 16:11:32 +0200

    [file]{information.blade.php}=> create information codes

--- resources/views/components/partials/footer/information.blade.php
+++ resources/views/components/partials/footer/information.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- Order your soul. Reduce your wants. - Augustine -->
+<div class="col-md-4 col-sm-4 col-xs-12">
+    <div class="footer-content">
+        <div class="footer-head">
+            <h4>information</h4>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
+            </p>
+            <div class="footer-contacts">
+                <p><span>Tel:</span> +123 456 789</p>
+                <p><span>Email:</span> contact@example.com</p>
+                <p><span>Working Hours:</span> 9am-5pm</p>
+            </div>
+        </div>
+    </div>
 </div>
+<!-- end single footer -->
