53e394fc91f40f51965cb71a25be9edeabb997ab
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-03 16:07:10 +0200

    [file]{footer.blade.php}=> delete about codes

--- resources/views/components/partials/footer.blade.php
+++ resources/views/components/partials/footer.blade.php
@@ -3,33 +3,7 @@
     <div class="footer-area">
         <div class="container">
             <div class="row">
-                <div class="col-md-4 col-sm-4 col-xs-12">
-                    <div class="footer-content">
-                        <div class="footer-head">
-                            <div class="footer-logo">
-                                <h2><span>e</span>Business</h2>
-                            </div>
 
-                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.</p>
-                            <div class="footer-icons">
-                                <ul>
-                                    <li>
-                                        <a href="#"><i class="fa fa-facebook"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-twitter"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-google"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fa fa-pinterest"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <!-- end single footer -->
                 <div class="col-md-4 col-sm-4 col-xs-12">
                     <div class="footer-content">
