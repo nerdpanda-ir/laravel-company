0eff2ddb6ec4d0964d24c1fe17dd1e9f846a37e5
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-03 16:10:50 +0200

    [file]{footer.blade.php} => delete information codes

--- resources/views/components/partials/footer.blade.php
+++ resources/views/components/partials/footer.blade.php
@@ -4,23 +4,6 @@
         <div class="container">
             <div class="row">
 
-                <!-- end single footer -->
-                <div class="col-md-4 col-sm-4 col-xs-12">
-                    <div class="footer-content">
-                        <div class="footer-head">
-                            <h4>information</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
-                            </p>
-                            <div class="footer-contacts">
-                                <p><span>Tel:</span> +123 456 789</p>
-                                <p><span>Email:</span> contact@example.com</p>
-                                <p><span>Working Hours:</span> 9am-5pm</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- end single footer -->
                 <div class="col-md-4 col-sm-4 col-xs-12">
                     <div class="footer-content">
                         <div class="footer-head">
