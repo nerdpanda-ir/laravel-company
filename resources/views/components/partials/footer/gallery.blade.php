a6b57b901fd3f18a79399624d80609211dabde23
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-03 16:13:11 +0200

    [file]{gallery.blade.php}=> create gallery codes

--- resources/views/components/partials/footer/gallery.blade.php
+++ resources/views/components/partials/footer/gallery.blade.php
@@ -1,3 +1,15 @@
-<div>
-    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
+<div class="col-md-4 col-sm-4 col-xs-12">
+    <div class="footer-content">
+        <div class="footer-head">
+            <h4>Instagram</h4>
+            <div class="flicker-img">
+                <a href="#"><img src="img/portfolio/1.jpg" alt=""></a>
+                <a href="#"><img src="img/portfolio/2.jpg" alt=""></a>
+                <a href="#"><img src="img/portfolio/3.jpg" alt=""></a>
+                <a href="#"><img src="img/portfolio/4.jpg" alt=""></a>
+                <a href="#"><img src="img/portfolio/5.jpg" alt=""></a>
+                <a href="#"><img src="img/portfolio/6.jpg" alt=""></a>
+            </div>
+        </div>
+    </div>
 </div>
