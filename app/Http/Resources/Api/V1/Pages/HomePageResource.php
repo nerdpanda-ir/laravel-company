89670fc8841205ca1b77ccd719381e963314f8de
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-08 11:31:50 +0200

    [file]{HomePageResource.php}=> create HomePageaResource class

---
+++ app/Http/Resources/Api/V1/Pages/HomePageResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\Api\V1\Pages;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HomePageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
