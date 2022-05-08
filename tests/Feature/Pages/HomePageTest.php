f0d5719fc0e68a3619f0579f0c47edb601063521
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-08 15:33:54 +0200

    [file]{HomePageTest.php} => remove duplicated codes

--- tests/Feature/Pages/HomePageTest.php
+++ tests/Feature/Pages/HomePageTest.php
@@ -9,16 +9,17 @@ use Tests\PageTestCase as TestCase;
 class HomePageTest extends TestCase
 {
     protected string $pageRoute = 'home' ;
+
     public function test_home_page_should_have_200_status_code() :void {
         $testResponse = $this->get(
-            route('home')
+            route($this->pageRoute)
         );
         $testResponse->assertStatus(200);
     }
 
     public function test_home_page_should_rendered_index_view() :void {
         $testResponse = $this->get(
-            route('home')
+            route($this->pageRoute)
         );
         $testResponse->assertViewIs('index');
     }
