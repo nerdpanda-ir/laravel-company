537fdc26499570f1d4dac12c5b3ee98377413e68
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-08 15:29:59 +0200

    [file]{HomePageTest.php} => remove namespace

--- tests/Feature/Pages/HomePageTest.php
+++ tests/Feature/Pages/HomePageTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature\Pages;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use Illuminate\Testing\TestResponse;
 
 class HomePageTest extends TestCase
 {
