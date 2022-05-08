f82ebd3a8b477997b2479c208dde31dfb13e4e67
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-08 11:34:07 +0200

    [file]{HomePageApiController.php}=> use HomePageResource namespace

--- app/Http/Controllers/Api/V1/HomePageApiController.php
+++ app/Http/Controllers/Api/V1/HomePageApiController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Resources\Api\V1\Pages\HomePageResource;
 
 class HomePageApiController extends Controller
 {
     public function __invoke() {
-        
+
     }
 }
