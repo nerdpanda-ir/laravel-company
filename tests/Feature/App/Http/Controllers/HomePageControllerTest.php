1c15a794007b11fad7ea12270b5fd0bc570b94c8
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-08 10:16:38 +0200

    [file]{HomePageControllerTest.php}=> add alias for namespace !!!

--- tests/Feature/App/Http/Controllers/HomePageControllerTest.php
+++ tests/Feature/App/Http/Controllers/HomePageControllerTest.php
@@ -4,10 +4,11 @@ namespace Tests\Feature\App\Http\Controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\SingleActionControllerTestCase;
+use Tests\SingleActionControllerTestCase as TestCase;
 use App\Http\Controllers\HomeController ;
 use Illuminate\Contracts\View\View;
-class HomePageControllerTest extends SingleActionControllerTestCase
+
+class HomePageControllerTest extends TestCase
 {
     protected string $namespace = HomeController::class ;
     public function test_invoke_method_should_return_view(){
