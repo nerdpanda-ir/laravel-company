5c710fdbd0c17c1d90a242483e4d54e6c7fe8229
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-08 11:35:27 +0200

    [file]{HomePageApiController.php}=> create return type for __invoke method

--- app/Http/Controllers/Api/V1/HomePageApiController.php
+++ app/Http/Controllers/Api/V1/HomePageApiController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\Api\V1\Pages\HomePageResource;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class HomePageApiController extends Controller
 {
-    public function __invoke() {
+    public function __invoke() :JsonResource {
 
     }
 }
