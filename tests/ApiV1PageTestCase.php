d275ac26210ec7e521e6db99bca7373b228caf14
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-08 17:32:36 +0200

    [file]{ApiV1PageTestCase.php}=> remove  __construct  and initialize value for property in class !!!

--- tests/ApiV1PageTestCase.php
+++ tests/ApiV1PageTestCase.php
@@ -5,9 +5,7 @@ use Tests\PageTestHasPrefix ;
 
 class ApiV1PageTestCase extends PageTestCase {
     use \Tests\PageTestHasPrefix ;
-    public function __construct(?string $name = null, array $data = [], $dataName = '')
-    {
-        parent::__construct($name, $data, $dataName);
-        $this->pageRoutePrefix = 'api.v1.pages.';
-    }
+
+    protected string $pageRoutePrefix = 'api.v1.page.';
+
 }
