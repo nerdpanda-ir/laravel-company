e40847d70fa5c14ca2c7e5ebf78f0b4cf25607e9
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-08 15:33:05 +0200

    [file]{HomePageTest.php} => set value to $pageRoute property

--- tests/Feature/Pages/HomePageTest.php
+++ tests/Feature/Pages/HomePageTest.php
@@ -8,6 +8,7 @@ use Tests\PageTestCase as TestCase;
 
 class HomePageTest extends TestCase
 {
+    protected string $pageRoute = 'home' ;
     public function test_home_page_should_have_200_status_code() :void {
         $testResponse = $this->get(
             route('home')
