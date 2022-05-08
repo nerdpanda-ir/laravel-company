5d11602f85dafe838154f3e0daccda206c9b860b
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-08 17:22:19 +0200

    [file]{ApiV1PageTestCase.php}=> make value for property in __construct

--- tests/ApiV1PageTestCase.php
+++ tests/ApiV1PageTestCase.php
@@ -5,4 +5,9 @@ use Tests\PageTestHasPrefix ;
 
 class ApiV1PageTestCase extends PageTestCase {
     use \Tests\PageTestHasPrefix ;
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->pageRoutePrefix = 'api.v1.pages.';
+    }
 }
