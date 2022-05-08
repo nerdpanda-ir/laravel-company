1e21a12394cf81494cc7911955b92fadff8b6840
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-08 10:47:45 +0200

    [file]{HomePageApiControllerTest.php}=> set HomePageApiController namespace as value to $namespace property

--- tests/Feature/App/Http/Controllers/Api/V1/HomePageApiControllerTest.php
+++ tests/Feature/App/Http/Controllers/Api/V1/HomePageApiControllerTest.php
@@ -5,9 +5,9 @@ namespace Tests\Feature\App\Http\Controllers\Api\V1;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\SingleActionControllerTestCase as TestCase;
-use App\Http\Controllers\Api\V1\HomePageApiController; 
+use App\Http\Controllers\Api\V1\HomePageApiController;
 
 class HomePageApiControllerTest extends TestCase
 {
-
+    protected string $namespace = HomePageApiController::class ;
 }
