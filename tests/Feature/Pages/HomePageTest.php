a41394b0a3ccf9748cb8fc31426005651ff8aeac
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-08 15:31:16 +0200

    [file]{HomePageTest.php} => remove testCase nameSpace and use PageTestCase

--- tests/Feature/Pages/HomePageTest.php
+++ tests/Feature/Pages/HomePageTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature\Pages;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use Tests\PageTestCase as TestCase;
 
 class HomePageTest extends TestCase
 {
