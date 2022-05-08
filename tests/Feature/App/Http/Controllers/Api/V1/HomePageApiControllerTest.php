482ea455cd3f9d55aa690235adde0f9f11b0a30d
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-08 11:09:57 +0200

    [file]{HomePageApiControllerTest.php}=> create logic for test method !!!

--- tests/Feature/App/Http/Controllers/Api/V1/HomePageApiControllerTest.php
+++ tests/Feature/App/Http/Controllers/Api/V1/HomePageApiControllerTest.php
@@ -13,6 +13,8 @@ class HomePageApiControllerTest extends TestCase
     protected string $namespace = HomePageApiController::class ;
 
     public function test_invoke_method_should_return_JsonResource_object():void {
-
+        $controller = $this->app->make($this->namespace);
+        $invokeResult = $this->app->call([$controller,'__invoke']);
+        $this->assertInstanceOf(JsonResource::class , $invokeResult);
     }
 }
