0e2d7cf8517e7dc168547fbdc6176868e86c6ee4
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-09 13:04:14 +0200

    [file]{HomePageTest.php} => refactor

--- tests/Feature/Pages/HomePageTest.php
+++ tests/Feature/Pages/HomePageTest.php
@@ -11,12 +11,12 @@ class HomePageTest extends TestCase
     protected string $pageRoute = 'home' ;
 
     public function test_home_page_should_have_200_status_code() :void {
-        $testResponse = $this->sendGetRequestToPageRouteByRouteName();
+        $testResponse = $this->getRequestToPageRouteByRouteName();
         $testResponse->assertStatus(200);
     }
 
     public function test_home_page_should_rendered_index_view() :void {
-        $testResponse = $this->sendGetRequestToPageRouteByRouteName();
+        $testResponse = $this->getRequestToPageRouteByRouteName();
         $testResponse->assertViewIs('index');
     }
 }
