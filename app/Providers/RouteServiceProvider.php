e1f0654a0f7c28f99569b6f2817bc0fe12faa816
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-09 13:44:42 +0200

    [file]{RouteServiceProvider.php}=> create name prefix for api routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,6 +30,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::prefix('api/v1/')
+                ->name('api.v1.')
                 ->middleware('api')
                 ->group(base_path('routes/api.php'));
 
