cea9064835e840b14ce4749de6d48af0fb929664
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-09 13:00:48 +0200

    [file]{HomePageApiTest.php}=> code refactor

--- tests/Feature/Pages/Api/V1/HomePageApiTest.php
+++ tests/Feature/Pages/Api/V1/HomePageApiTest.php
@@ -11,24 +11,17 @@ class HomePageApiTest extends TestCase
     protected string $pageRoute = 'home';
 
     public function test_home_api_page_should_have_200_status_code():void {
-        /* @todo have duplicate code !!!  */
-        $testResponse = $this->get(
-            route($this->pageRouteWithPrefix())
-        );
+        $testResponse = $this->getRequestToFullPageRouteByRouteName();
         $testResponse->assertStatus(200);
     }
 
     public function test_content_type_header_in_home_api_page_response_should_is_application_json():void {
-        $testResponse = $this->get(
-            route($this->pageRouteWithPrefix())
-        );
+        $testResponse = $this->getRequestToFullPageRouteByRouteName();
         $testResponse->assertHeader('content-type','application/json');
     }
 
     public function test_home_api_page_json_response_body():void{
-        $testResponse = $this->get(
-            route($this->pageRouteWithPrefix())
-        );
+        $testResponse = $this->getRequestToFullPageRouteByRouteName();
         $testResponse->assertJson([
             'data'=> [
                 'message'=> 'welcome to home'
