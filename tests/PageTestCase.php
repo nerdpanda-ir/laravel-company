b19a3548de67c38b4f0c2e4fec6be4638a2bcf2f
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-09 11:10:15 +0200

    [file]{PageTestCase.php}=>  create  method

--- tests/PageTestCase.php
+++ tests/PageTestCase.php
@@ -1,9 +1,19 @@
 <?php namespace Tests ; ?>
 <?php
+use Illuminate\Testing\TestResponse;
 use Tests\TestCase ;
+
 class PageTestCase extends TestCase {
     protected string $pageRoute;
     protected function pageRoute():string {
         return $this->pageRoute;
     }
+
+    protected function sendBasicGetRequestByRouteName(array $headers = []):TestResponse {
+        return $this->get(
+            route($this->pageRoute()) ,
+            $headers
+        );
+    }
+
 }
