68a3fd2164531f4117334071215c335707c40170
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-10 07:06:25 +0200

    [file]{ApiV1PageTestCase.php}=> class no use trait

--- tests/ApiV1PageTestCase.php
+++ tests/ApiV1PageTestCase.php
@@ -4,7 +4,7 @@ use Tests\PageTestCase ;
 use Tests\PageTestHasPrefix ;
 
 class ApiV1PageTestCase extends PageTestCase {
-    use \Tests\PageTestHasPrefix ;
+    
 
     protected string $pageRoutePrefix = 'api.v1.page.';
 
