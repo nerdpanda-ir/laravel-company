db38f5a4dd28c2c1dff806884bfa6ca09a787340
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-10 08:05:14 +0200

    [file]{BlogControllerTest.php}=> replace class parent

--- tests/Feature/App/Http/Controllers/BlogControllerTest.php
+++ tests/Feature/App/Http/Controllers/BlogControllerTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature\App\Http\Controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use Tests\ControllerTestCase as TestCase ;
 
 class BlogControllerTest extends TestCase
 {
