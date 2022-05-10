969c89425412b675aae2cd6521e26a5e760c8ea5
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-10 08:06:12 +0200

    [file]{BlogControllerTest.php}=> use BlogController NameSpace

--- tests/Feature/App/Http/Controllers/BlogControllerTest.php
+++ tests/Feature/App/Http/Controllers/BlogControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\App\Http\Controllers;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\ControllerTestCase as TestCase ;
+use App\Http\Controllers\BlogController;
 
 class BlogControllerTest extends TestCase
 {
