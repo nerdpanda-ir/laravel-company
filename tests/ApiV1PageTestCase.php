5c39349b7c7a0b8070c5bfd792c7eeb4211c0a9a
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-10 07:06:46 +0200

    [file]{ApiV1PageTestCase.php}=> remove trait namespace

--- tests/ApiV1PageTestCase.php
+++ tests/ApiV1PageTestCase.php
@@ -1,10 +1,9 @@
 <?php namespace Tests ; ?>
 <?php
 use Tests\PageTestCase ;
-use Tests\PageTestHasPrefix ;
+
 
 class ApiV1PageTestCase extends PageTestCase {
-    
 
     protected string $pageRoutePrefix = 'api.v1.page.';
 
