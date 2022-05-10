f02a619b44cb8e1c63aba1bd477aa6f87ffd5ec1
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-10 13:09:45 +0200

    [file]{BlogControllerTest.php}=> create test method

--- tests/Feature/App/Http/Controllers/BlogControllerTest.php
+++ tests/Feature/App/Http/Controllers/BlogControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\ControllerTestCase as TestCase ;
@@ -11,5 +12,12 @@ class BlogControllerTest extends TestCase {
     protected string $namespace = BlogController::class ;
     public function test_index_method_should_exist():void {
         $this->assertControllerHasMethod('index');
+    }
+    public function test_index_method_should_return_instance_of_View() :void {
+        /** @todo create global call */
+        /** @todo assert return instance -> for all methods !!! */
+        $controller = $this->controllerInstance();
+        $indexResult = $this->app->call([$controller,'index']);
+        $this->assertInstanceOf(View::class,$indexResult);
     }
 }
