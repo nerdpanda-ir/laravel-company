29d1ddfa1a3f7424c45af28f5aa9271c19077a0c
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-11 12:08:09 +0200

    [file]{BlogPageTest.php}=> change testClass parent !!!

--- tests/Feature/Pages/BlogPageTest.php
+++ tests/Feature/Pages/BlogPageTest.php
@@ -4,9 +4,9 @@ namespace Tests\Feature\Pages;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use Tests\PageWithPrefixTestCase as TestCase ;
 
 class BlogPageTest extends TestCase
 {
-    
+
 }
