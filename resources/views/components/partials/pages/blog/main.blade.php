8012dfae9d67bede4ee44e230f43ffe1c573219d
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-12 08:03:27 +0200

    [file]{main.blade.php}=> create codes for main partial

--- resources/views/components/partials/pages/blog/main.blade.php
+++ resources/views/components/partials/pages/blog/main.blade.php
@@ -1,3 +1,2 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+<x-partials.pages.blog.blog-area />
+<div class="clearfix"></div>
