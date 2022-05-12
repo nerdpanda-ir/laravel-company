e54ad505ba6f9e626538c58541e3c0aee075c102
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-12 05:29:50 +0200

    [file]{BlogPageTest.php}-> refactor method !!!

--- tests/Feature/Pages/BlogPageTest.php
+++ tests/Feature/Pages/BlogPageTest.php
@@ -11,8 +11,6 @@ class BlogPageTest extends TestCase
     protected string $pageRoutePrefix = 'blog.';
     protected string $pageRoute = 'index';
     public function test_blogPage_should_have_200_status_code():void {
-        /* @todo i can merge to one method !!! */
-        $testResponse = $this->getRequestToFullPageRouteByRouteName();
-        $testResponse->assertStatus(200);
+        $this->assertOkFullPageRouteGetResponseThatReceivedByRouteName();
     }
 }
