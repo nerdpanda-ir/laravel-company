ce74b9338a3e35a7fad22d72ba6183e8fd0b4b64
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-12 08:08:59 +0200

    [file]{blog.blade.php}=> remove footer component and backToTop component !!!

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.front')
 @section('title') صفحه وبلاگ @endsection
 @section('main')
-    
+
     <x-partials.header />
     <x-partials.pages.blog.main />
-    <x-partials.footer />
-    <x-back-to-top />
+
 
 @endsection
