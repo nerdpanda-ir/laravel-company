74c300eb017e96afa4056c643510dbe2198c7a1d
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-14 11:33:17 +0200

    [file]{BlogPageApiTest.php}=> create test method

--- tests/Feature/Pages/Api/V1/BlogPageApiTest.php
+++ tests/Feature/Pages/Api/V1/BlogPageApiTest.php
@@ -13,4 +13,13 @@ class BlogPageApiTest extends TestCase
     public function test_should_blog_api_page_have_200_status_code():void {
         $this->assertOkFullPageRouteGetResponseThatReceivedByRouteName();
     }
+
+    public function test_content_type_header_in_blog_page_api_response_should_is_application_json():void {
+        /** @todo can merge this methods to one */
+        /** @todo after merge => should refactor home page api test */
+        $testResponse = $this->getRequestToFullPageRouteByRouteName();
+        $testResponse->assertHeader('content-type','application/json');
+    }
+
+
 }
