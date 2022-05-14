810f82338158e1d8e8bb3611fc4869718fd0d2f4
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-14 15:19:13 +0200

    [file]{BlogCotrollerTest.php}=> refactor test method

--- tests/Feature/App/Http/Controllers/BlogControllerTest.php
+++ tests/Feature/App/Http/Controllers/BlogControllerTest.php
@@ -11,9 +11,7 @@ use App\Http\Controllers\BlogController;
 class BlogControllerTest extends TestCase {
     protected string $namespace = BlogController::class ;
     public function test_invoke_method_should_return_instance_of_View() :void {
-        /** @todo create method in controller test case -> for method should return instance of view !!! */
-        /** @todo create method in single action controller test case -> invoke method should return instance of view !!! */
-        $this->assertInvokeMethodReturnInstanceOf(View::class);
+        $this->assertInvokeMethodReturnViewInstance();
     }
 
     public function test_invoke_method_should_return_blog_view():void {
