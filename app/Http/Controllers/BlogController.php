efd88547f79232cefaf994819195926173cb72c1
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-14 05:31:08 +0200

    [file]{BlogController.php}=> create __invoke method for controller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -11,4 +11,7 @@ class BlogController extends Controller
         $data = ['language'=>'en' , 'charset'=>'utf88888'];
         return \view('blog',$data);
     }
+    public function __invoke(){
+        
+    }
 }
