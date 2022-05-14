2e9960ec475b916de55e0706631046f7bdf352cd
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-14 09:29:07 +0200

    [file]{BlogPageApiControllerTest.php}=> add test method

--- tests/Feature/App/Http/Controllers/Api/V1/BlogPageApiControllerTest.php
+++ tests/Feature/App/Http/Controllers/Api/V1/BlogPageApiControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\App\Http\Controllers\Api\V1;
 
+use App\Http\Resources\Api\V1\Pages\BlogPageResource;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -16,5 +17,10 @@ class BlogPageApiControllerTest extends TestCase
         /** @todo create method in Single Action controller test cast -> check __invoke returned jsonResource */
         /** @todo use method to all */
         $this->assertInvokeMethodReturnInstanceOf(JsonResource::class);
+    }
+
+    public function test_invoke_method_should_return_instance_of_BlogPageResource():void{
+        /* @todo add this method for other page api test class !!! */
+        $this->assertInvokeMethodReturnInstanceOf(BlogPageResource::class);
     }
 }
