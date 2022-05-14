c8b29926fc099e8f8764e5f9062cc556c0848467
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-14 09:53:08 +0200

    [file]{BlogPageApiTest.php}=> create test method

--- tests/Feature/Pages/Api/V1/BlogPageApiTest.php
+++ tests/Feature/Pages/Api/V1/BlogPageApiTest.php
@@ -8,5 +8,9 @@ use Tests\ApiV1PageTestCase as TestCase;
 
 class BlogPageApiTest extends TestCase
 {
-    protected string $pageRoute = 'blog'; 
+    protected string $pageRoute = 'blog';
+
+    public function test_should_blog_api_page_have_200_status_code():void {
+        $this->assertOkFullPageRouteGetResponseThatReceivedByRouteName();
+    }
 }
