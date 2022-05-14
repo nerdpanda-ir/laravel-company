b9363f42aec01e531285afbbdd8f834320b9670d
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-14 08:28:50 +0200

    [file]{BlogPageApiControllerTest.php}=> change parent for test class

--- tests/Feature/App/Http/Controllers/Api/V1/BlogPageApiControllerTest.php
+++ tests/Feature/App/Http/Controllers/Api/V1/BlogPageApiControllerTest.php
@@ -4,9 +4,9 @@ namespace Tests\Feature\App\Http\Controllers\Api\V1;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use Tests\SingleActionControllerTestCase as TestCase ;
 
 class BlogPageApiControllerTest extends TestCase
 {
-    
+
 }
