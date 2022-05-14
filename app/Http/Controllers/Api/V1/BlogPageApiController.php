389dd5ff008ed911851ff37f86e68acb05bd4541
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-14 09:04:12 +0200

    [file]{BlogPageApiController.php}=> invoke method return BlogPageResource

--- app/Http/Controllers/Api/V1/BlogPageApiController.php
+++ app/Http/Controllers/Api/V1/BlogPageApiController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\V1\Pages\BlogPageResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class BlogPageApiController extends Controller
 {
     public function __invoke():JsonResource {
-        
+        return BlogPageResource::make([]);
     }
 }
