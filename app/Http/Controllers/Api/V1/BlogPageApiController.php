5a8711b45fad3750e5d506b26c12b10ded3b79da
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-14 09:01:19 +0200

    [file]{BlogPageApiController.php}=> add type hint for invoke method

--- app/Http/Controllers/Api/V1/BlogPageApiController.php
+++ app/Http/Controllers/Api/V1/BlogPageApiController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class BlogPageApiController extends Controller
 {
-    public function __invoke() {
-
+    public function __invoke():JsonResource {
+        
     }
 }
