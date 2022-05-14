1ae47fa3e1d0f2423a1397f1360cd286c59a096e
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-14 07:37:48 +0200

    [file]{BlogPageTest.php}=>  change  assertion method for test method

--- tests/Feature/Pages/BlogPageTest.php
+++ tests/Feature/Pages/BlogPageTest.php
@@ -9,11 +9,14 @@ use Tests\PageTestCase as TestCase ;
 class BlogPageTest extends TestCase
 {
     protected string $pageRoute = 'blog';
-    public function test_blogPage_should_have_200_status_code():void {
-        /** @todo review all test class for apply this assertion methods !!!! */
-        $this->assertOkFullPageRouteGetResponseThatReceivedByRouteName();
-    }
+//    public function test_blogPage_should_have_200_status_code():void {
+//        /** @todo review all test class for apply this assertion methods !!!! */
+//        $this->assertOkFullPageRouteGetResponseThatReceivedByRouteName();
+//    }
 
+    public function test_blog_page_should_have_200_status_code():void {
+        $this->assertOkPageRouteGetResponseThatReceivedViaRouteName();
+    }
     public function test_blogPage_should_rendered_blogView():void {
         /** @todo maybe merge to one method !!!*/
         $testResponse = $this->getRequestToFullPageRouteByRouteName();
