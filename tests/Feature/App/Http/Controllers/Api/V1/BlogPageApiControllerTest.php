9399d27f1387951831eba76dd559cf9844b0cb12
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-14 08:30:00 +0200

    [file]{BlogPageApiControllerTest.php}=> use missed controller

--- tests/Feature/App/Http/Controllers/Api/V1/BlogPageApiControllerTest.php
+++ tests/Feature/App/Http/Controllers/Api/V1/BlogPageApiControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\App\Http\Controllers\Api\V1;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\SingleActionControllerTestCase as TestCase ;
+use App\Http\Controllers\Api\V1\BlogPageApiController ;
 
 class BlogPageApiControllerTest extends TestCase
 {
