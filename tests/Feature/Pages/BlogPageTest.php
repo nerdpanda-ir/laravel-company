b310733c13cdc9901eecacd66c0d50d6873afd21
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-14 07:30:28 +0200

    [file]{BlogPageTest.php}=> modify property

--- tests/Feature/Pages/BlogPageTest.php
+++ tests/Feature/Pages/BlogPageTest.php
@@ -8,7 +8,7 @@ use Tests\PageTestCase as TestCase ;
 
 class BlogPageTest extends TestCase
 {
-    protected string $pageRoute = 'index';
+    protected string $pageRoute = 'blog';
     public function test_blogPage_should_have_200_status_code():void {
         /** @todo review all test class for apply this assertion methods !!!! */
         $this->assertOkFullPageRouteGetResponseThatReceivedByRouteName();
