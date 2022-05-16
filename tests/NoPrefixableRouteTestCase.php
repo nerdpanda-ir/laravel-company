eb65b4f3cf8531882cf8020c684027ada185025e
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-16 16:55:54 +0200

    [file]{NoPrefixableRouteTestCase.php}=> create methods !!!

--- tests/NoPrefixableRouteTestCase.php
+++ tests/NoPrefixableRouteTestCase.php
@@ -1,5 +1,30 @@
 <?php namespace Tests ; ?>
 <?php
-class NoPrefixableRouteTestCase extends RouteTestCase {
 
+use Illuminate\Testing\TestResponse;
+
+class NoPrefixableRouteTestCase extends RouteTestCase {
+    protected string $pageRoute;
+
+    protected function pageRoute():string {
+        return $this->pageRoute;
+    }
+
+    protected function getRequestToPageRouteByRouteName(
+        array $routeParameters = [] ,
+        array $headers = []
+    ):TestResponse {
+        return $this->get(
+            route( $this->pageRoute() , $routeParameters ) ,
+            $headers
+        );
+    }
+
+    protected function assertOkPageRouteGetResponseThatReceivedViaRouteName(
+        array $routeParameters = [] ,
+        array $headers = [] ,
+    ):void {
+        $testResponse = $this->getRequestToPageRouteByRouteName( $routeParameters , $headers );
+        $testResponse->assertOk();
+    }
 }
