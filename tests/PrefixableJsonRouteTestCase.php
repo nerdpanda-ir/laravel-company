396f18e82111c428e1335b6d49c2129fd84ebe92
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-17 16:16:53 +0200

    [file]{PrefixableJsonRouteTestCase.php}=> create assertion method

--- tests/PrefixableJsonRouteTestCase.php
+++ tests/PrefixableJsonRouteTestCase.php
@@ -2,4 +2,11 @@
 <?php
 class PrefixableJsonRouteTestCase extends PrefixableRouteTestCase {
     use JsonableRoute;
+
+    protected function assertValueForContentTypeHeaderInReceivedResponseFromGetRequestToFullPageRouteByRouteNameEqualToApplicationJson(
+        array $routeParameters = [] , array $headers = [] ,
+    ):void {
+        $testResponse = $this->getRequestToFullPageRouteByRouteName($routeParameters , $headers );
+        $this->assertValueForContentTypeHeaderInResponseEqualToApplicationJson($testResponse);
+    }
 }
