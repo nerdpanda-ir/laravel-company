2a6f1a5b8cce6131d03d8ca455a4c62d810fce22
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-17 06:47:56 +0200

    [file]{PrefixableViewRouteTestCase.php}=> create assertion method

--- tests/PrefixableViewRouteTestCase.php
+++ tests/PrefixableViewRouteTestCase.php
@@ -1,5 +1,10 @@
 <?php namespace Tests ; ?>
 <?php
 class PrefixableViewRouteTestCase extends PrefixableRouteTestCase {
-    
+    protected function assertViewInReceivedGetResponseFromRequestToFullPageRouteByRouteNameIs(
+        string $view , array $routeParameters = [] , array $headers = [] ,
+    ):void {
+        $testResponse = $this->getRequestToFullPageRouteByRouteName( $routeParameters , $headers );
+        $testResponse->assertViewIs($view);
+    }
 }
