ca2503e2f17c50c9ff4c9a4fbceb5ca3481b6ec1
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-17 05:34:10 +0200

    [file]{NoPrefixableViewRouteTestCase.php}=> create methods

--- tests/NoPrefixableViewRouteTestCase.php
+++ tests/NoPrefixableViewRouteTestCase.php
@@ -1,5 +1,17 @@
 <?php namespace Tests ; ?>
 <?php
 class NoPrefixableViewRouteTestCase extends NoPrefixableRouteTestCase {
-
+    protected function assertViewInReceivedGetResponseFromRequestToPageRouteByRouteNameIs(
+        string $view , array $routeParameters = [] , array $headers = [] ,
+    ):void {
+        /** @todo create view page test
+        create assert for text html
+         *
+         * may be automaticaly run ->
+         * create json page test
+         * create assert for application/json
+         */
+        $testResponse = $this->getRequestToPageRouteByRouteName($routeParameters , $headers) ;
+        $testResponse->assertViewIs($view);
+    }
 }
