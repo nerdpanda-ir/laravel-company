a2fef212dd8a82d92bdfd933a75e8fb5fdadc53f
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-17 08:21:16 +0200

    [file]{ViewableRoute.php}=> create assertion method

--- tests/ViewableRoute.php
+++ tests/ViewableRoute.php
@@ -1,5 +1,10 @@
 <?php namespace Tests ;?>
 <?php
+
+use Illuminate\Testing\TestResponse;
+
 trait ViewableRoute {
-    
+    protected function assertViewInResponseEqualTo(TestResponse $response , string $view ):void {
+        $response->assertViewIs($view);
+    }
 }
