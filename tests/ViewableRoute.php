676ac60161fff93b9cfc4a9406780689707ee117
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-17 14:08:31 +0200

    [file]{ViewableRoute.php}=> create assertion method

--- tests/ViewableRoute.php
+++ tests/ViewableRoute.php
@@ -7,4 +7,8 @@ trait ViewableRoute {
     protected function assertViewInResponseEqualTo(TestResponse $response , string $view ):void {
         $response->assertViewIs($view);
     }
+
+    protected function assertValueForContentTypeHeaderInResponseEqualToTextHtml(TestResponse $response):void {
+        $this->assertValueForContentTypeHeaderEqualTo($response, 'text/html; charset=UTF-8');
+    }
 }
