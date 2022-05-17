73e427302ba666e1e7b921bf9d40dabae7ac5d8c
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-17 09:37:10 +0200

    [file]{RouteTestCase.php}=> create assertion method

--- tests/RouteTestCase.php
+++ tests/RouteTestCase.php
@@ -1,9 +1,19 @@
 <?php namespace Tests ;?>
 <?php
+
+use Illuminate\Testing\TestResponse;
+
 class RouteTestCase extends TestCase {
     protected string $pageRoute;
 
     protected function pageRoute():string {
         return $this->pageRoute;
     }
+
+    protected function assertHeaderValueInResponseEqualTo(
+        TestResponse $response , string $header , string $value ,
+    ):void {
+        $response->assertHeader($header,$value);
+    }
+
 }
