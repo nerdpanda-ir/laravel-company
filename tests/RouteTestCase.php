d584f68182f31c8f2a33f6cd7a7c63953e606e7c
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-17 05:25:49 +0200

    [file]{RouteTestCase.php}=> create property and method

--- tests/RouteTestCase.php
+++ tests/RouteTestCase.php
@@ -1,5 +1,9 @@
 <?php namespace Tests ;?>
 <?php
 class RouteTestCase extends TestCase {
+    protected string $pageRoute;
 
+    protected function pageRoute():string {
+        return $this->pageRoute;
+    }
 }
