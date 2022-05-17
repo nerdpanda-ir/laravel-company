eea7ce54d591ecf24bb9e7539a144c094f283f03
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-17 05:29:21 +0200

    [file]{PrefixableRouteTestCase.php}=> create methods

--- tests/PrefixableRouteTestCase.php
+++ tests/PrefixableRouteTestCase.php
@@ -1,5 +1,42 @@
 <?php namespace Tests ; ?>
 <?php
-class PrefixableRouteTestCase extends RouteTestCase {
 
+use Illuminate\Testing\TestResponse;
+
+class PrefixableRouteTestCase extends RouteTestCase {
+    protected string $pageRoutePrefix;
+
+    protected function pageRoutePrefix() :string {
+        return $this->pageRoutePrefix;
+    }
+
+    protected function pageRouteGenerateWithPrefix(string $pageRoute):string {
+        return $this->pageRoutePrefix.$pageRoute;
+    }
+
+    protected function pageRouteWithPrefix():string {
+        return $this->pageRouteGenerateWithPrefix($this->pageRoute);
+    }
+
+    /**
+     * this method send get request to pageRoutePrefix +  page route by route name !!!!
+     * @param array $routeParameters
+     * @param array $headers
+     * @return TestResponse
+     */
+    protected function getRequestToFullPageRouteByRouteName(
+        array $routeParameters = [] , array $headers = [] ,
+    ):TestResponse {
+        return $this->get(
+            route($this->pageRouteWithPrefix(), $routeParameters ) ,
+            $headers
+        );
+    }
+
+    protected function assertOkFullPageRouteGetResponseThatReceivedByRouteName(
+        array $routeParameters = [] ,  array $headers = [] ,
+    ):void {
+        $testResponse = $this->getRequestToFullPageRouteByRouteName($routeParameters,$headers);
+        $testResponse->assertOk();
+    }
 }
