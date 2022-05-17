747de45b59bdfb37a588dffcf6afab2ff9a73931
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-17 10:19:31 +0200

    [file]{JsonableRoute.php}=> create assertion method

--- tests/JsonableRoute.php
+++ tests/JsonableRoute.php
@@ -1,5 +1,12 @@
 <?php namespace Tests ; ?>
 <?php
+
+use Illuminate\Testing\TestResponse;
+
 trait JsonableRoute{
-    
+    protected function assertValueForContentTypeHeaderInResponseEqualToApplicationJson(
+        TestResponse $response
+    ) :void {
+        $this->assertValueForContentTypeHeaderEqualTo($response,'application/json');
+    }
 }
