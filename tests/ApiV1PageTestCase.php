3cbc37c5ea5277dc06ae52d8b76dac99e939af36
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-17 06:29:34 +0200

    [file]{ApiV1PageTestCase.php}=> delete file !!!

--- tests/ApiV1PageTestCase.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Tests ; ?>
-<?php
-use Tests\PageWithPrefixTestCase as TestCase ;
-class ApiV1PageTestCase extends TestCase {
-    protected string $pageRoutePrefix = 'api.v1.page.';
-}
