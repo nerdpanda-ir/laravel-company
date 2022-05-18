8859f0c801b117ec635aa5d0717343233fab3655
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-18 13:41:39 +0200

    [file]{HomePageApiControllerTest.php}=> add new test method

--- tests/Feature/App/Http/Controllers/Api/V1/HomePageApiControllerTest.php
+++ tests/Feature/App/Http/Controllers/Api/V1/HomePageApiControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\App\Http\Controllers\Api\V1;
 
+use App\Http\Resources\Api\V1\Pages\HomePageResource;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\SingleActionControllerTestCase as TestCase;
@@ -14,5 +15,9 @@ class HomePageApiControllerTest extends TestCase
 
     public function test_invoke_method_should_return_JsonResource_object():void {
         $this->assertInvokeMethodReturnJsonResourceInstance();
+    }
+
+    public function test_invoke_method_should_return_instance_of_HomePageResource():void {
+        $this->assertInvokeMethodReturnInstanceOf(HomePageResource::class);
     }
 }
