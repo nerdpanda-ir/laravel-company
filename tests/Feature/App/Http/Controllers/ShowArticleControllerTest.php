00664041e3161a12e55bf5dfa5ed026ebcf47fde
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-19 04:56:34 +0200

    [file]{ShowArticleControllerTest.php}=> use missing controller

--- tests/Feature/App/Http/Controllers/ShowArticleControllerTest.php
+++ tests/Feature/App/Http/Controllers/ShowArticleControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\App\Http\Controllers;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\SingleActionControllerTestCase as TestCase;
+use App\Http\Controllers\ShowArticleController;
 
 class ShowArticleControllerTest extends TestCase
 {
