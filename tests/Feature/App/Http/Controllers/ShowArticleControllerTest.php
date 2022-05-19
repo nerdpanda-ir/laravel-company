a0ba7e2658b4cfb4a4394b28cd3627b00c0c4a8f
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-19 04:53:58 +0200

    [file]{ShowArticleControllerTest.php}=> use single action controller test namespace and change test class parent !!!

--- tests/Feature/App/Http/Controllers/ShowArticleControllerTest.php
+++ tests/Feature/App/Http/Controllers/ShowArticleControllerTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature\App\Http\Controllers;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use Tests\SingleActionControllerTestCase as TestCase;
 
 class ShowArticleControllerTest extends TestCase
 {
