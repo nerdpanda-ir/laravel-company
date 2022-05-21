74492d777fdca496b12904a7984c55ad1adc4dcc
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 13:47:31 +0200

    [file]{MethodsTest.php}=> create test method

--- tests/Unit/HasNamespaceSetterInterface/MethodsTest.php
+++ tests/Unit/HasNamespaceSetterInterface/MethodsTest.php
@@ -7,5 +7,14 @@ use App\Contracts\HasNamespaceSetterInterface;
 
 class MethodsTest extends TestCase
 {
-    protected string $namespace = HasNamespaceSetterInterface::class ; 
+    protected string $namespace = HasNamespaceSetterInterface::class ;
+    public function test_should_have_single_method():void {
+        $interfaceReflection = new \ReflectionClass($this->namespace);
+        $methods = $interfaceReflection->getMethods();
+        $methodsCount = count($methods);
+        $this->assertEquals(
+            1 , $methodsCount ,
+            "interface $this->namespace just should have single method not more !!! "
+        );
+    }
 }
