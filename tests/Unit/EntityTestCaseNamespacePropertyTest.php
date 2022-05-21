6beb71d4ce37f69f8eb2abc5e4e4e4597e9ecf4b
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 08:14:06 +0200

    [file]{EntityTestCaseNamespacePropertyTest.php}=> create test method

--- tests/Unit/EntityTestCaseNamespacePropertyTest.php
+++ tests/Unit/EntityTestCaseNamespacePropertyTest.php
@@ -17,4 +17,10 @@ class EntityTestCaseNamespacePropertyTest extends TestCase
         $isProtected = $namespaceInfo->isProtected();
         $this->assertTrue($isProtected,"should access modifier for \$namespace property in class => $this->namespace is protected !!! ");
     }
+    public function test_should_is_string():void{
+        $namespaceReflection = new \ReflectionProperty($this->namespace , 'namespace');
+        $realType = (string)$namespaceReflection->getType();
+        $expected = 'string';
+        $this->assertEquals($expected ,$realType,"data type for \$namespace property should is string !!! ");
+    }
 }
