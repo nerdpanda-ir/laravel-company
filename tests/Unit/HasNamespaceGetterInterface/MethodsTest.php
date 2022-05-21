ef53da82ba7d5e0342a336b83dc59ee468f5a812
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 12:46:29 +0200

    [file]{MethodsTest.php}=> use interface namespace

--- tests/Unit/HasNamespaceGetterInterface/MethodsTest.php
+++ tests/Unit/HasNamespaceGetterInterface/MethodsTest.php
@@ -3,8 +3,9 @@
 namespace Tests\Unit\HasNamespaceGetterInterface;
 
 use PHPUnit\Framework\TestCase;
+use App\Contracts\HasNamespaceGetterInterface;
 
 class MethodsTest extends TestCase
 {
-    
+
 }
