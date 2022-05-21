e509914537c1db064f7ce4c81ecf6b2d13edcbf8
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 14:21:59 +0200

    [file]{NamespaceMethodParametersTest.php}=> create test method

--- tests/Unit/HasNamespaceGetterInterface/NamespaceMethodParametersTest.php
+++ tests/Unit/HasNamespaceGetterInterface/NamespaceMethodParametersTest.php
@@ -9,4 +9,12 @@ class NamespaceMethodParametersTest extends TestCase
 {
     protected string $namespace = HasNamespaceGetterInterface::class ;
     protected string $method = 'namespace';
+    public function test_never_exist_parameter():void {
+        $methodReflection = new \ReflectionMethod($this->namespace,$this->method);
+        $parametersCount = $methodReflection->getNumberOfParameters() ;
+        $this->assertEquals(
+            0 , $parametersCount ,
+            "method $this->method() from interface $this->namespace should have 0 parameter"
+        );
+    }
 }
