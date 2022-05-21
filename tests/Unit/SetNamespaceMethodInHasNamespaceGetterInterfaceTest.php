65fd2ad808d45cdcbf898c19ba05e542aea93431
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 10:48:13 +0200

    [file]{SetNamespaceMethodInHasNamespaceGetterInterfaceTest.php}=> create test method

--- tests/Unit/SetNamespaceMethodInHasNamespaceGetterInterfaceTest.php
+++ tests/Unit/SetNamespaceMethodInHasNamespaceGetterInterfaceTest.php
@@ -9,4 +9,11 @@ class SetNamespaceMethodInHasNamespaceGetterInterfaceTest extends TestCase
 {
     protected string $namespace = HasNamespaceSetterInterface::class ;
     protected string $method = 'setNamespace';
+    public function test_method_exist():void {
+        $exist = method_exists($this->namespace , $this->method);
+        $this->assertTrue(
+            $exist ,
+            "interface => $this->namespace should have $this->method() method !!! "
+        );
+    }
 }
