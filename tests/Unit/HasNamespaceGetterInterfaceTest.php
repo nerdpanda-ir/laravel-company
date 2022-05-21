733e045bb930f06325cbb253be372b73a379d906
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 08:37:07 +0200

    [file]{HasNamespaceGetterInterfaceTest.php}=> create test method

--- tests/Unit/HasNamespaceGetterInterfaceTest.php
+++ tests/Unit/HasNamespaceGetterInterfaceTest.php
@@ -8,4 +8,8 @@ use App\Contracts\HasNamespaceGetterInterface;
 class HasNamespaceGetterInterfaceTest extends TestCase
 {
     protected string $namespace = HasNamespaceGetterInterface::class ;
+    public function test_is_exist():void {
+        $isExist = interface_exists($this->namespace) ;
+        $this->assertTrue($isExist,"doesnt exist interface $this->namespace");
+    }
 }
