61ce1473d0e6c3dde0a6d4100735e828d7bd6dc8
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 03:47:43 +0200

    [file]{ControllerTestCase.php}=> create todo comment

--- tests/ControllerTestCase.php
+++ tests/ControllerTestCase.php
@@ -16,7 +16,7 @@ class ControllerTestCase extends TestCase {
             $this->messageForMissingController()
         );
     }
-
+    //todo create message repository class , methods in message repository use laravel lang
     protected function messageForMissingController(...$parameters):string {
         return "we not have {$this->namespace} class !!!";
     }
@@ -24,11 +24,11 @@ class ControllerTestCase extends TestCase {
     protected function controllerInstance(array $controllerArgs = []) :object {
         return $this->app->make($this->namespace,$controllerArgs);
     }
-
+    //@todo create controller method test case | class method test case
     protected function controllerHasMethod(string $method) :bool {
         return method_exists($this->namespace , $method) ;
     }
-
+    //@todo move to controller method test case | class method test case
     protected function assertControllerHasMethod(
         string $method ,
         string $message = '' ,
@@ -146,8 +146,7 @@ class ControllerTestCase extends TestCase {
             View::class , $method , $methodArgs , $message
         );
     }
-
-
+    
     protected function assertNoneStaticMethodInControllerReturnViewInstance(
         string $method , array $methodArgs = [] ,
         array $controllerArgs = [] , string $message = '' ,
