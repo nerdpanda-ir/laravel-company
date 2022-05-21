d113c89097a5296cbd68f72eec868df129771da6
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 07:57:38 +0200

    [file]{EntityTestCaseNamespacePropertyTest.php}=> rename method

--- tests/Unit/EntityTestCaseNamespacePropertyTest.php
+++ tests/Unit/EntityTestCaseNamespacePropertyTest.php
@@ -8,7 +8,7 @@ use Tests\EntityTestCase;
 class EntityTestCaseNamespacePropertyTest extends TestCase
 {
     protected string $namespace = EntityTestCase::class ;
-    public function test_should_exist_namespace_property_in_EntityTestCase_class ():void {
+    public function test_should_exist_namespace_property_class ():void {
         $isExist = property_exists($this->namespace,'namespace');
         $this->assertTrue($isExist,"no exist \$namespace property in $this->namespace ");
     }
