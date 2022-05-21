56533153f01b28d83a6bfaa24e2592df9c2cd65f
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 09:00:00 +0200

    [file]{NamespaceMethodInHasNamespaceGetterInterfaceTest.php}=> use interface namespace

--- tests/Unit/NamespaceMethodInHasNamespaceGetterInterfaceTest.php
+++ tests/Unit/NamespaceMethodInHasNamespaceGetterInterfaceTest.php
@@ -3,8 +3,9 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use App\Contracts\HasNamespaceGetterInterface;
 
 class NamespaceMethodInHasNamespaceGetterInterfaceTest extends TestCase
 {
-   
+
 }
