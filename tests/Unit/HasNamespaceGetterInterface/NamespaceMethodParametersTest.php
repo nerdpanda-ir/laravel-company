652e567a52e10de708cea7e0ddf38052bb041565
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 14:18:34 +0200

    [file]{NamespaceMethodParametersTest.php}=> rename class

--- tests/Unit/HasNamespaceGetterInterface/NamespaceMethodParametersTest.php
+++ tests/Unit/HasNamespaceGetterInterface/NamespaceMethodParametersTest.php
@@ -5,7 +5,7 @@ namespace Tests\Unit\HasNamespaceGetterInterface;
 use PHPUnit\Framework\TestCase;
 use App\Contracts\HasNamespaceGetterInterface;
 
-class ParametersTest extends TestCase
+class NamespaceMethodParametersTest extends TestCase
 {
     protected string $namespace = HasNamespaceGetterInterface::class ;
     protected string $method = 'namespace';
