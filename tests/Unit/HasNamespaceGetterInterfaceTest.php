e15a0a5bdcad3aa878acba4ae7c46b4997691609
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 08:34:17 +0200

    [file]{HasNamespaceGetterInterfaceTest.php}=> clean test class

--- tests/Unit/HasNamespaceGetterInterfaceTest.php
+++ tests/Unit/HasNamespaceGetterInterfaceTest.php
@@ -7,13 +7,5 @@ use App\Contracts\HasNamespaceGetterInterface;
 
 class HasNamespaceGetterInterfaceTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $this->assertTrue(true);
-    }
+    
 }
