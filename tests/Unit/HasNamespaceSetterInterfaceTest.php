dca4a16731dd21c86b3dc56f341c7cc8d6f29176
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 10:15:02 +0200

    [file]{HasNamespaceSetterInterfaceTest.php}=> create test class

--- tests/Unit/HasNamespaceSetterInterfaceTest.php
+++ tests/Unit/HasNamespaceSetterInterfaceTest.php
@@ -8,5 +8,11 @@ use App\Contracts\HasNamespaceSetterInterface ;
 class HasNamespaceSetterInterfaceTest extends TestCase
 {
     protected string $namespace = HasNamespaceSetterInterface::class ;
-    
+    public function test_is_exist():void {
+        $isExist = interface_exists($this->namespace);
+        $this->assertTrue(
+            $isExist ,
+            "doesnt exist {$this->namespace} interface !!!"
+        );
+    }
 }
