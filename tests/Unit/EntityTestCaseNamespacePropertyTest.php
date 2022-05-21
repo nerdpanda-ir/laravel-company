8a63b63d8849035a9da9b90c9abcca309362242f
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 07:22:36 +0200

    [file]{EntityTestCaseNamespacePropertyTest.php}=> clean test class

--- tests/Unit/EntityTestCaseNamespacePropertyTest.php
+++ tests/Unit/EntityTestCaseNamespacePropertyTest.php
@@ -4,15 +4,8 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 use Tests\EntityTestCase;
+
 class EntityTestCaseNamespacePropertyTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $this->assertTrue(true);
-    }
+
 }
