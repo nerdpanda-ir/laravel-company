8182da2b62d2ef7ab2c98d43c7bead6a58486a3c
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 07:39:48 +0200

    [file]{EntityTestCaseClassTest.php}=> create test method

--- tests/Unit/EntityTestCaseClassTest.php
+++ tests/Unit/EntityTestCaseClassTest.php
@@ -4,7 +4,7 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 use Tests\EntityTestCase;
-
+use Tests\TestCase as LaravelTestCase;
 class EntityTestCaseClassTest extends TestCase
 {
     protected string $namespace = EntityTestCase::class ;
@@ -15,5 +15,9 @@ class EntityTestCaseClassTest extends TestCase
             $isExist ,
             " no exist $this->namespace class !!! "
         );
+    }
+    public function test_EntityTestCaseClass_should_extend_from_TestCase():void {
+        $isExtend = get_parent_class($this->namespace) ;
+        $this->assertEquals(LaravelTestCase::class ,$isExtend," class {$this->namespace} should extend ".LaravelTestCase::class .' class !!!');
     }
 }
