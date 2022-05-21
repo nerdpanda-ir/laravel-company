7b8c4767c86d63506fd3863fc58582a08f76df68
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 10:38:46 +0200

    [file]{HasNamespaceSetterInterfaceTest.php}=> create method

--- tests/Unit/HasNamespaceSetterInterfaceTest.php
+++ tests/Unit/HasNamespaceSetterInterfaceTest.php
@@ -15,4 +15,9 @@ class HasNamespaceSetterInterfaceTest extends TestCase
             "doesnt exist {$this->namespace} interface !!!"
         );
     }
+
+    public function test_never_implement():void {
+        $implements = class_implements($this->namespace);
+        $this->assertEmpty($implements,"interface -> $this->namespace never implement other interfaces ");
+    }
 }
