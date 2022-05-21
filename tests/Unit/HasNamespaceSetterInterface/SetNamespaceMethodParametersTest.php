67c1f9f220197ba873da23e623023fdec29a8fe7
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 14:25:16 +0200

    [file]{SetNamespaceMethodParametersTest.php}=> use interface namespace

--- tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodParametersTest.php
+++ tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodParametersTest.php
@@ -3,8 +3,9 @@
 namespace Tests\Unit\HasNamespaceSetterInterface;
 
 use PHPUnit\Framework\TestCase;
+use App\Contracts\HasNamespaceSetterInterface;
 
 class SetNamespaceMethodParametersTest extends TestCase
 {
-   
+
 }
