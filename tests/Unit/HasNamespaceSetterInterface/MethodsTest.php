79a2d0fe4f6cccfdda9831442ce23d0af9c9a48c
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-21 13:39:35 +0200

    [file]{MethodsTest.php}=> create file

---
+++ tests/Unit/HasNamespaceSetterInterface/MethodsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\HasNamespaceSetterInterface;
+
+use PHPUnit\Framework\TestCase;
+
+class MethodsTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
