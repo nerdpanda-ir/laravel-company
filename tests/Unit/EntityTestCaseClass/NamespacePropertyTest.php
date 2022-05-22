f7adfb5b09830ddbd22d835ee1af0cd48d4f2314
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 06:59:24 +0200

    [file]{NamespaceProperyTest.php}=> create todo comment

--- tests/Unit/EntityTestCaseClass/NamespacePropertyTest.php
+++ tests/Unit/EntityTestCaseClass/NamespacePropertyTest.php
@@ -9,7 +9,7 @@ class NamespacePropertyTest extends TestCase
 {
     protected string $namespace = EntityTestCase::class ;
     protected string $property = 'namespace';
-
+    //@todo can create propertiable interface , for class test , trait test, ...
     public function test_should_exist ():void {
         $isExist = property_exists($this->namespace,$this->property);
         $this->assertTrue($isExist,"no exist \$namespace property in $this->namespace ");
@@ -19,7 +19,9 @@ class NamespacePropertyTest extends TestCase
         $isProtected = $namespaceInfo->isProtected();
         $this->assertTrue($isProtected,"should access modifier for \$namespace property in class => $this->namespace is protected !!! ");
     }
+    
     public function test_should_is_string():void{
+        //@todo check with union type -> may be have bug
         $namespaceReflection = new \ReflectionProperty($this->namespace , 'namespace');
         $realType = (string)$namespaceReflection->getType();
         $expected = 'string';
