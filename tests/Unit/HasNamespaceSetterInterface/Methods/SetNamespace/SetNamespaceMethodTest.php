f5e0bb27ba9faba71cf1402f1fe941b77df9231e
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 08:10:49 +0200

    [file]{SetNamespaceMethodsTest.php}=> modify namespace

--- tests/Unit/HasNamespaceSetterInterface/Methods/SetNamespace/SetNamespaceMethodTest.php
+++ tests/Unit/HasNamespaceSetterInterface/Methods/SetNamespace/SetNamespaceMethodTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit\HasNamespaceSetterInterface;
+namespace Tests\Unit\HasNamespaceSetterInterface\Methods\SetNamespace;
 
 use PHPUnit\Framework\TestCase;
 use App\Contracts\HasNamespaceSetterInterface;
