31be80120598d7186ccf85e88476f567ed5fa8be
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 06:02:24 +0200

    [file]{SetNamespaceMethodNamespaceParameterTest.php}=> create test method

--- tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodNamespaceParameterTest.php
+++ tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodNamespaceParameterTest.php
@@ -19,4 +19,33 @@ class SetNamespaceMethodNamespaceParameterTest extends TestCase
         $exist = in_array($this->parameter, $parameters);
         $this->assertTrue($exist, "method => $this->method () from interface => $this->namespace should have \$$this->parameter");
     }
+    public function test_parameter_data_type_should_is_string():void {
+        // strict check , normal check -> (in array )
+        //this is strict check !!!
+        $methodReflection = new \ReflectionMethod($this->namespace,$this->method);
+        $parameters = $methodReflection->getParameters();
+        //@todo create custom method reflection service -> extend from main  method reflection class an create method for get parameters with datatype !!!
+        $parametersWithTypes = [] ;
+        $expect = ['string'];
+        sort($expect,SORT_STRING);
+        foreach ($parameters as $parameter)
+            if ($parameter->hasType())
+                $parametersWithTypes[$parameter->name] = explode('|',(string)$parameter->getType());
+            else
+                $parametersWithTypes[$parameter->name] = null ;
+        if (array_key_exists($this->parameter,$parametersWithTypes))  {
+            $parameterDataTypes = $parametersWithTypes[$this->parameter] ;
+            if (is_array($parameterDataTypes)) {
+                sort($parameterDataTypes,SORT_STRING);
+                $this->assertEquals(
+                    implode('|',$expect) ,
+                    implode('|',$parameterDataTypes) ,
+                );
+            }
+            else if (is_null($parameterDataTypes))
+                $this->fail("parameter $$this->parameter in method $this->method() should is ".implode('|',$expect).' !!! but no typehinted !!!');
+        }
+        else
+            $this->fail("in method : $this->method() doesnt exist $$this->parameter parameter !!!");
+    }
 }
