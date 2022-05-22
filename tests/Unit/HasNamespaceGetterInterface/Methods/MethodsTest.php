39ab73eda9bf0b5773e4b86f719018c7b1d6d7a3
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 13:01:10 +0200

    [file]{MethodsTest.php}=> modify todo comment and create test method

--- tests/Unit/HasNamespaceGetterInterface/Methods/MethodsTest.php
+++ tests/Unit/HasNamespaceGetterInterface/Methods/MethodsTest.php
@@ -17,5 +17,19 @@ class MethodsTest extends TestCase
             "interface $this->namespace should only have one method !!!"
         );
     }
-    //@todo create test methods for static , none staticm method
+
+    public function test_no_have_static_method():void{
+        $interfaceReflection = new \ReflectionClass($this->namespace);
+        $methods = $interfaceReflection->getMethods();
+        $staticMethods = [];
+        foreach ($methods as $method)
+            if ($method->isStatic())
+                $staticMethods[] = $method ;
+        $staticMethodCount = count($staticMethods);
+        $this->assertEquals(
+             $staticMethodCount , $staticMethodCount ,
+            "interface $this->namespace no have any static method !!! "
+        );
+    }
+    //@todo create test methods for static , none staticm method , abstaract check , none abstarct , final , ..
 }
