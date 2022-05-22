232d9fc616572403ffea0331b780899d7632b4cb
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 04:26:40 +0200

    [file]{SetNamespaceMethodNamespaceParameterTest.php}=> create property in test class

--- tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodNamespaceParameterTest.php
+++ tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodNamespaceParameterTest.php
@@ -7,5 +7,6 @@ use App\Contracts\HasNamespaceSetterInterface;
 class SetNamespaceMethodNamespaceParameterTest extends TestCase
 {
     protected string $namespace = HasNamespaceSetterInterface::class ;
-    
+    protected string $method = 'setNamespace';
+   
 }
