60aab391a9070492b90b4f4ce5c7e018f8e21cf1
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 15:10:51 +0200

    [file]{MethpdsTest.php}=> create test method

--- tests/Unit/NamespaceableInterface/MethodsTest.php
+++ tests/Unit/NamespaceableInterface/MethodsTest.php
@@ -8,4 +8,17 @@ use App\Contracts\NamespaceableInterface ;
 class MethodsTest extends TestCase
 {
     protected string $namespace = NamespaceableInterface::class ;
+    public function test_should_no_have_method():void {
+        $interfaceReflection = new \ReflectionClass($this->namespace);
+        $methods = $interfaceReflection->getMethods();
+        $interfaceMethods = [];
+        foreach ($methods as $method)
+            if ($method->class==$this->namespace)
+                $interfaceMethods[] = $method ;
+        $interfaceMethodsCount = count($interfaceMethods);
+        $this->assertEquals(
+            0 , $interfaceMethodsCount ,
+            "$this->namespace should no declare method !!! "
+        );
+    }
 }
