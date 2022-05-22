34e67f3f0407b70db6813440e7ac9956aa052945
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 14:36:00 +0200

    [file]{NamespaceableInterfaceTest.php}=> create test method

--- tests/Unit/NamespaceableInterface/NamespaceableInterfaceTest.php
+++ tests/Unit/NamespaceableInterface/NamespaceableInterfaceTest.php
@@ -8,4 +8,11 @@ use App\Contracts\NamespaceableInterface;
 class NamespaceableInterfaceTest extends TestCase
 {
     protected string $namespace = NamespaceableInterface::class ;
+    public function test_is_exist():void {
+        $isExist = interface_exists($this->namespace);
+        $this->assertTrue(
+            $isExist ,
+            "$this->namespace interface doesnt exist !!! "
+        );
+    }
 }
