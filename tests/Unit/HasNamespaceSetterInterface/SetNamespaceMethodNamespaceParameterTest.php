f749e89268f9b7816cbcd7a76682e9ebd5932821
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 04:17:09 +0200

    [file]{SetNamespaceMethodNamespaceParameterTest.php}=> create test class for test special parameter in method

---
+++ tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodNamespaceParameterTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\HasNamespaceSetterInterface;
+
+use PHPUnit\Framework\TestCase;
+
+class SetNamespaceMethodNamespaceParameterTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
