0ad0e6e166b802a1d4bb93267701bc5e41013a67
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 06:13:28 +0200

    [file]{SetNamespaceMethodTest.php}=> create test method

--- tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodTest.php
+++ tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodTest.php
@@ -16,5 +16,14 @@ class SetNamespaceMethodTest extends TestCase
             "interface => $this->namespace should have $this->method() method !!! "
         );
     }
-    //@todo return type should be check
+
+    public function test_should_method_is_void(): void {
+        $methodReflection = new \ReflectionMethod($this->namespace,$this->method);
+        $returnType = (string)$methodReflection->getReturnType();
+        $expected = 'void';
+        $this->assertEquals(
+            $expected, $returnType ,
+            "method $this->method() from interface $this->namespace should is void !!!"
+        );
+    }
 }
