a90cce150df880a9cc90e12ab89b17dd2729f8bb
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 12:43:01 +0200

    [file]{EntityTestCase.php}=> create test method and modify todo comment

--- tests/Unit/EntityTestCaseClass/EntityTestCaseClassTest.php
+++ tests/Unit/EntityTestCaseClass/EntityTestCaseClassTest.php
@@ -5,7 +5,7 @@ namespace Tests\Unit\EntityTestCaseClass;
 use PHPUnit\Framework\TestCase;
 use Tests\EntityTestCase;
 use Tests\TestCase as LaravelTestCase;
-//@todo -> class test || service test -> create method for check isabstract
+//@todo -> class test || service test -> create method for check isabstract || final
 class EntityTestCaseClassTest extends TestCase
 {
     protected string $namespace = EntityTestCase::class ;
@@ -20,5 +20,13 @@ class EntityTestCaseClassTest extends TestCase
     public function test_EntityTestCaseClass_should_extend_from_TestCase():void {
         $isExtend = get_parent_class($this->namespace) ;
         $this->assertEquals(LaravelTestCase::class ,$isExtend," class {$this->namespace} should extend ".LaravelTestCase::class .' class !!!');
+    }
+    public function test_no_final():void {
+        $classReflection = new \ReflectionClass($this->namespace);
+        $isNoFinal = !$classReflection->isFinal();
+        $this->assertTrue(
+            $isNoFinal ,
+            "$this->namespace class dont be final "
+        );
     }
 }
