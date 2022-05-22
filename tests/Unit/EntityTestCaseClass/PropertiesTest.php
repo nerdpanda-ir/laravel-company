2944cf2fb1469fee75a453e3320f346c20a52cde
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 06:42:49 +0200

    [file]{PropertiesTest.php}=> create test class

--- tests/Unit/EntityTestCaseClass/PropertiesTest.php
+++ tests/Unit/EntityTestCaseClass/PropertiesTest.php
@@ -8,4 +8,19 @@ use Tests\EntityTestCase;
 class PropertiesTest extends TestCase
 {
     protected string $namespace = EntityTestCase::class ;
+    public function test_should_only_single_property():void {
+        $classReflection = new \ReflectionClass($this->namespace);
+        $properties = $classReflection->getProperties();
+        $classProperties = [];
+        foreach ($properties as $property)
+            if ($property->class==$this->namespace)
+                $classProperties[] = $property->name;
+        $classPropertiesCount = count($classProperties);
+        $expected = 1 ;
+        $this->assertEquals(
+            $expected ,
+            $classPropertiesCount ,
+            "class : $this->namespace just should have single property !!! "
+        );
+    }
 }
