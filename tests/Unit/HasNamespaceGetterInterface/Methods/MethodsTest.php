32e159eb6acafcdacddb3e5f394b0050dd9f676b
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 07:44:39 +0200

    [file]{MethodsTest.php}=> modify namespace

--- tests/Unit/HasNamespaceGetterInterface/Methods/MethodsTest.php
+++ tests/Unit/HasNamespaceGetterInterface/Methods/MethodsTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit\HasNamespaceGetterInterface;
+namespace Tests\Unit\HasNamespaceGetterInterface\Methods;
 
 use PHPUnit\Framework\TestCase;
 use App\Contracts\HasNamespaceGetterInterface;
