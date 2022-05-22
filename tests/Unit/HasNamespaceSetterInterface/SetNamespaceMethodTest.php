f1f3ed3f3e97b6058a790b2ca9afddfdfe820956
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 04:29:21 +0200

    [file]{SetNamespaceMethodTest.php}=> remove unrelated method

--- tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodTest.php
+++ tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodTest.php
@@ -17,14 +17,4 @@ class SetNamespaceMethodTest extends TestCase
         );
     }
     //@todo return type should be check
-    //@todo create method parameter test class , parametr position check , parameter count
-    public function test_method_should_have_namespace_parameter():void {
-        $methodReflection = new \ReflectionMethod($this->namespace , $this->method);
-        $requiredParameter = 'namespace';
-        $parameters = [] ;
-        foreach ($methodReflection->getParameters() as  $parameter)
-            $parameters[] = $parameter->name;
-        $exist = in_array($requiredParameter , $parameters );
-        $this->assertTrue($exist,"method => $this->method () from interface => $this->namespace should have \$$requiredParameter");
-    }
 }
