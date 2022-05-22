43f9cfcf47a3a3b9d3745b6f130e3812c7dabad8
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-22 04:30:07 +0200

    [file]{SetNamespaceMethodNamespaceParameterTest.php}=> create test method

--- tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodNamespaceParameterTest.php
+++ tests/Unit/HasNamespaceSetterInterface/SetNamespaceMethodNamespaceParameterTest.php
@@ -9,4 +9,14 @@ class SetNamespaceMethodNamespaceParameterTest extends TestCase
     protected string $namespace = HasNamespaceSetterInterface::class ;
     protected string $method = 'setNamespace';
     protected string $parameter = 'namespace';
+    //@todo create method parameter test class , parametr position check , parameter count , parameter type check
+    public function test_parameter_is_exist(): void
+    {
+        $methodReflection = new \ReflectionMethod($this->namespace, $this->method);
+        $parameters = [];
+        foreach ($methodReflection->getParameters() as $parameter)
+            $parameters[] = $parameter->name;
+        $exist = in_array($this->parameter, $parameters);
+        $this->assertTrue($exist, "method => $this->method () from interface => $this->namespace should have \$$this->parameter");
+    }
 }
