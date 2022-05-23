2bedba7a0779133850511d2b56710a47d821a8f0
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 10:23:59 +0200

    [file]{EntityTestCaseInterface}=> using interface namespace

--- tests/Unit/EntityTestCaseInterface/EntityTestCaseInterface.php
+++ tests/Unit/EntityTestCaseInterface/EntityTestCaseInterface.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\EntityTestCaseInterface;
 
 use PHPUnit\Framework\TestCase;
+use App\Contracts\Tests\EntityTestCaseInterface;
 
 class EntityTestCaseInterface extends TestCase
 {
