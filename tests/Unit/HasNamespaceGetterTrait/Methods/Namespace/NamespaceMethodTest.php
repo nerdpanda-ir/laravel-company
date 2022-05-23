02d9530ed77fa82c446d239d09d6d46ac280914f
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 15:49:17 +0200

    [file]{NamespaceMethodTest.php}=> create test method

--- tests/Unit/HasNamespaceGetterTrait/Methods/Namespace/NamespaceMethodTest.php
+++ tests/Unit/HasNamespaceGetterTrait/Methods/Namespace/NamespaceMethodTest.php
@@ -33,4 +33,20 @@ class NamespaceMethodTest extends TestCase
         $isNotAbstract = !$methodReflection->isAbstract();
         $this->assertTrue($isNotAbstract,"method $this->method() in $this->namespace trait dont be abstract !!!");
     }
+    public function test_should_just_have_string_return_type():void{
+        $expect = ['string','int'];
+        sort($expect,SORT_STRING);
+        $expect = implode('|',$expect);
+        $methodReflection = new \ReflectionMethod($this->namespace , $this->method);
+        $returnTypes = (string)$methodReflection->getReturnType();
+        if (strlen($returnTypes)!=0){
+            $returnTypes = explode('|',$returnTypes);
+            sort($returnTypes,SORT_STRING);
+            $returnTypes = implode('|',$returnTypes);
+            $this->assertEquals($expect , $returnTypes  , "return type for method : $this->method() in $this->namespace should is $expect but is $returnTypes");
+        }
+        else
+            $this->fail("no return type detect for method $this->method() in $this->namespace trait !!! should return type is $expect");
+
+    }
 }
