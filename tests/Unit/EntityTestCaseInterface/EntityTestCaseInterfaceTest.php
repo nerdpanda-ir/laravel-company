2094859d79362b19b9fb097cfc4ea228cc1d6499
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 10:29:33 +0200

    [file]{EntityTestCaseInterfaceTest.php}=> create test method

--- tests/Unit/EntityTestCaseInterface/EntityTestCaseInterfaceTest.php
+++ tests/Unit/EntityTestCaseInterface/EntityTestCaseInterfaceTest.php
@@ -8,5 +8,8 @@ use App\Contracts\Tests\EntityTestCaseInterface;
 class EntityTestCaseInterfaceTest extends TestCase
 {
     protected string $namespace = EntityTestCaseInterface::class ;
-
+    public function test_is_exist():void {
+        $isExist = interface_exists($this->namespace);
+        $this->assertTrue($isExist,"interface $this->namespace doesnt exsit !!!");
+    }
 }
