27e88eff6af843b8c337c0b1cfa02e4dc4447430
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 14:50:42 +0200

    [file]{NamespaceMethodTest.php}=> create property

--- tests/Unit/HasNamespaceGetterTrait/Methods/Namespace/NamespaceMethodTest.php
+++ tests/Unit/HasNamespaceGetterTrait/Methods/Namespace/NamespaceMethodTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Unit\HasNamespaceGetterTrait\Methods\Namespace;
 
+use App\Traits\HasNamespaceGetterTrait;
 use PHPUnit\Framework\TestCase;
 
 class NamespaceMethodTest extends TestCase
 {
-
+    protected string $namespace = HasNamespaceGetterTrait::class;
 }
