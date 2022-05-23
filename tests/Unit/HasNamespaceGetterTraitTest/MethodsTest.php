bf85b9ee68e5b44a2e6e77b5d95a71687d54f36a
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 13:44:45 +0200

    [file]{MethodsTest.php}=> create test method

--- tests/Unit/HasNamespaceGetterTraitTest/MethodsTest.php
+++ tests/Unit/HasNamespaceGetterTraitTest/MethodsTest.php
@@ -15,4 +15,23 @@ class MethodsTest extends TestCase
         $expect = 1 ;
         $this->assertEquals($expect,$methodsCount,"$this->namespace trait should have just one method ");
     }
+    public function test_should_no_have_static_method():void {
+        $traitReflection = new \ReflectionClass($this->namespace);
+        $methods = $traitReflection->getMethods();
+        $staticMethods = array_filter($methods,function (\ReflectionMethod $method){
+            return $method->isStatic();
+        });
+        $staticMethods[] = 'ez';
+        $staticMethods[] = 'gg';
+        $staticMethodsCount = sizeof($staticMethods);
+        $message = '';
+        if (!empty($staticMethods)) {
+            if ($staticMethodsCount==1)
+                $message = "method $staticMethods[0]() ";
+            else
+                $message = 'methods '.implode('() , ',$staticMethods).'() , ';
+            $message .= "dont be static !!! in $this->namespace trait ";
+        }
+        $this->assertEquals(0,$staticMethodsCount,$message);
+    }
 }
