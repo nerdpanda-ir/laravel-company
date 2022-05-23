b0c38c48cf2608c136b53df71e37a3cc2d02688d
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 15:31:26 +0200

    [file]{ParametersTest.php}=> create test class

--- tests/Unit/HasNamespaceGetterTrait/Methods/Namespace/ParametersTest.php
+++ tests/Unit/HasNamespaceGetterTrait/Methods/Namespace/ParametersTest.php
@@ -8,5 +8,10 @@ use PHPUnit\Framework\TestCase;
 class ParametersTest extends TestCase
 {
     protected string $namespace = HasNamespaceGetterTrait::class ;
-    protected string $method = 'namespace'; 
+    protected string $method = 'namespace';
+    public function test_no_have_any_parameter():void {
+        $methodReflection = new \ReflectionMethod($this->namespace , $this->method);
+        $parameters = $methodReflection->getParameters();
+        $this->assertEmpty($parameters , "$this->method() in $this->namespace trait :  should no have any parameter !!! ");
+    }
 }
