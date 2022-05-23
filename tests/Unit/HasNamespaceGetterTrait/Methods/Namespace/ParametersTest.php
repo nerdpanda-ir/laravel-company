a4ffe3bb636702fd3c5d27457b2b23b7f3154da5
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 15:29:02 +0200

    [file]{ParametersTest.php}=> define property

--- tests/Unit/HasNamespaceGetterTrait/Methods/Namespace/ParametersTest.php
+++ tests/Unit/HasNamespaceGetterTrait/Methods/Namespace/ParametersTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Unit\HasNamespaceGetterTrait\Methods\Namespace;
 
+use App\Traits\HasNamespaceGetterTrait;
 use PHPUnit\Framework\TestCase;
 
 class ParametersTest extends TestCase
 {
-    
+    protected string $namespace = HasNamespaceGetterTrait::class ;
+
 }
