004adf09ed9aa770072a2c4060e873cfb53999d4
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 15:27:52 +0200

    [file]{ParametersTest.php}=> create test class

---
+++ tests/Unit/HasNamespaceGetterTrait/Methods/Namespace/ParametersTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\HasNamespaceGetterTrait\Methods\Namespace;
+
+use PHPUnit\Framework\TestCase;
+
+class ParametersTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
