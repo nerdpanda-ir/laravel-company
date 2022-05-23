1f0fa3047565ae465395ff2f5f613c485730fb77
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 10:22:39 +0200

    [file]{EntityTestCaseInterface}=> create test class

---
+++ tests/Unit/EntityTestCaseInterface/EntityTestCaseInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\EntityTestCaseInterface;
+
+use PHPUnit\Framework\TestCase;
+
+class EntityTestCaseInterface extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
