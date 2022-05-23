40239b629bf526c518366cfb2a68c016a9aa6496
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 10:38:40 +0200

    [file]{EntityTestCaseInterfaceTest.php}=> create test method

--- tests/Unit/EntityTestCaseInterface/EntityTestCaseInterfaceTest.php
+++ tests/Unit/EntityTestCaseInterface/EntityTestCaseInterfaceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\EntityTestCaseInterface;
 
 use PHPUnit\Framework\TestCase;
 use App\Contracts\Tests\EntityTestCaseInterface;
+use App\Contracts\NamespaceableInterface;
 
 class EntityTestCaseInterfaceTest extends TestCase
 {
@@ -11,5 +12,11 @@ class EntityTestCaseInterfaceTest extends TestCase
     public function test_is_exist():void {
         $isExist = interface_exists($this->namespace);
         $this->assertTrue($isExist,"interface $this->namespace doesnt exsit !!!");
+    }
+
+    public function test_should_extend_one_interface():void {
+        $implements = class_implements($this->namespace);
+        $implementsCount = sizeof($implements);
+        $this->assertEquals(2,$implementsCount,"$this->namespace interface should just extend from one interface !!!");
     }
 }
