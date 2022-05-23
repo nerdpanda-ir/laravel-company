a703d7e408c2ce80595c803f011ef18b696b4120
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 15:28:23 +0200

    [file]{ParametersTest.php}=> clean test class

--- tests/Unit/HasNamespaceGetterTrait/Methods/Namespace/ParametersTest.php
+++ tests/Unit/HasNamespaceGetterTrait/Methods/Namespace/ParametersTest.php
@@ -6,13 +6,5 @@ use PHPUnit\Framework\TestCase;
 
 class ParametersTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $this->assertTrue(true);
-    }
+    
 }
