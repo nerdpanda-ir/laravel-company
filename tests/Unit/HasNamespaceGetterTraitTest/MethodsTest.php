6fb3968a0b56a37126a7098dee2aacbd1018d6a8
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 13:37:56 +0200

    [file]{MethodsTest.php}=> create test method

--- tests/Unit/HasNamespaceGetterTraitTest/MethodsTest.php
+++ tests/Unit/HasNamespaceGetterTraitTest/MethodsTest.php
@@ -8,4 +8,11 @@ use App\Traits\HasNamespaceGetterTrait;
 class MethodsTest extends TestCase
 {
     protected string $namespace = HasNamespaceGetterTrait::class;
+    public function test_should_have_one_method():void {
+        $traitReflection = new \ReflectionClass($this->namespace);
+        $methods = $traitReflection->getMethods();
+        $methodsCount = sizeof($methods);
+        $expect = 1 ;
+        $this->assertEquals($expect,$methodsCount,"$this->namespace trait should have just one method ");
+    }
 }
