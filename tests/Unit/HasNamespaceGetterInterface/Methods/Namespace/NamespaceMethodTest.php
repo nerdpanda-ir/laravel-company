d4d5beffbe23fe202c20ed946ea4cb2088e17fc4
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-23 06:32:17 +0200

    [file]{NamespaceMethodTest.php}=> create test method

--- tests/Unit/HasNamespaceGetterInterface/Methods/Namespace/NamespaceMethodTest.php
+++ tests/Unit/HasNamespaceGetterInterface/Methods/Namespace/NamespaceMethodTest.php
@@ -14,6 +14,12 @@ class NamespaceMethodTest extends TestCase
         $isExist = method_exists($this->namespace,$this->method);
         $this->assertTrue($isExist, " should interface => $this->namespace has $this->method() method");
     }
+    public function test_is_not_static():void {
+        $methodReflection = new \ReflectionMethod($this->namespace , $this->method );
+        $isNotStatic = !$methodReflection->isStatic() ;
+        $this->assertTrue($isNotStatic, "method $this->method() from $this->namespace dont be static !!! ");
+    }
+    
     public function test_return_type_is_string():void {
         // @todo have bug here when method return union !!!
         $methodReflection = new \ReflectionMethod($this->namespace,$this->method);
