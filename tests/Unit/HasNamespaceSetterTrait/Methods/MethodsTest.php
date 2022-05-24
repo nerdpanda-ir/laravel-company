9c4c567a6e54afe5a8ec5750e419c55fa27eea08
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 04:17:05 +0200

    [file]{MethodsTest.php}=> define test method

--- tests/Unit/HasNamespaceSetterTrait/Methods/MethodsTest.php
+++ tests/Unit/HasNamespaceSetterTrait/Methods/MethodsTest.php
@@ -8,5 +8,10 @@ use App\Traits\HasNamespaceSetterTrait;
 class MethodsTest extends TestCase
 {
     protected string $namespace = HasNamespaceSetterTrait::class ;
-    
+    public function test_should_only_have_one_method():void {
+        $traitReflection = new \ReflectionClass($this->namespace);
+        $methods = $traitReflection->getMethods();
+        $methodsCount = count($methods);
+        $this->assertEquals(0,$methodsCount);
+    }
 }
