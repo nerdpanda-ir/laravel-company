27f9ff9ab19dce5ed99415782c6a8b60435d9738
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 13:47:36 +0200

    [file]{NamespaceMethodTest.php}=> create test class

---
+++ tests/Unit/EntityTestCaseClass/Methods/Namespace/NamespaceMethodTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Unit\EntityTestCaseClass\Methods\Namespace;
+
+use PHPUnit\Framework\TestCase;
+use Tests\EntityTestCase;
+
+class NamespaceMethodTest extends TestCase
+{
+    protected string $namespace = EntityTestCase::class;
+    protected string $method = 'namespace';
+    public function test_is_exist():void {
+        $isExist = method_exists($this->namespace , $this->method);
+        $this->assertTrue($isExist," $this->namespace class should have $this->method() method !!");
+    }
+    public function test_is_public():void {
+        $methodReflection = new \ReflectionMethod($this->namespace,$this->method);
+        $isPublic = $methodReflection->isPublic();
+        $this->assertTrue($isPublic," $this->method() method in $this->namespace class should is public !!! ");
+    }
+    public function test_never_static():void {
+        $methodReflection = new \ReflectionMethod($this->namespace , $this->method);
+        $isNotStatic = !$methodReflection->isStatic();
+        $this->assertTrue($isNotStatic,"method $this->method() in $this->namespace class should is none static !!!");
+    }
+    public function test_never_final():void {
+        $methodReflection = new \ReflectionMethod($this->namespace , $this->method);
+        $isNotFinal = !$methodReflection->isFinal();
+        $this->assertTrue($isNotFinal,"method $this->method() in $this->namespace class dont be final !!! ");
+    }
+    public function test_never_abstract():void {
+        $methodReflection = new \ReflectionMethod($this->namespace , $this->method);
+        $isNotAbstract = !$methodReflection->isAbstract();
+        $this->assertTrue($isNotAbstract,"method $this->method() in $this->namespace class dont be abstract !!!");
+    }
+    public function test_should_have_return_type():void {
+        $methodReflection = new \ReflectionMethod($this->namespace,$this->method);
+        $hasReturnType = $methodReflection->hasReturnType() ;
+        $this->assertTrue($hasReturnType,"no detect return value for  $this->method() in $this->namespace class ");
+    }
+    public function test_should_just_have_string_return_type():void{
+        $expect = ['string'];
+        sort($expect,SORT_STRING);
+        $expect = implode('|',$expect);
+        $methodReflection = new \ReflectionMethod($this->namespace , $this->method);
+        $returnTypes = (string)$methodReflection->getReturnType();
+        if (strlen($returnTypes)!=0){
+            $returnTypes = explode('|',$returnTypes);
+            sort($returnTypes,SORT_STRING);
+            $returnTypes = implode('|',$returnTypes);
+            $this->assertEquals($expect , $returnTypes  , "return type for method : $this->method() in $this->namespace class should is $expect but is $returnTypes");
+        }
+        else
+            $this->fail("no return type detect for method $this->method() in $this->namespace class !!! should return type is $expect");
+
+    }
+}
