343a05197fba9122e17a72f971b8e954d45f4f83
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 05:33:30 +0200

    [file]{SetNamespaceTest.php}=> use trait namespace

--- tests/Unit/HasNamespaceSetterTrait/Methods/SetNamespace/SetNamespaceTest.php
+++ tests/Unit/HasNamespaceSetterTrait/Methods/SetNamespace/SetNamespaceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\HasNamespaceSetterTrait\Methods\SetNamespace;
 
 use PHPUnit\Framework\TestCase;
+use App\Traits\HasNamespaceSetterTrait;
 
 class SetNamespaceTest extends TestCase
 {
