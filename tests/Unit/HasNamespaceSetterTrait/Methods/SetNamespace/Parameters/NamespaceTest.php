672ad08dc74bc348a3dbefd117715ddef837a9e8
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 06:55:38 +0200

    [file]{NamespaceTest.php}=> create test method

--- tests/Unit/HasNamespaceSetterTrait/Methods/SetNamespace/Parameters/NamespaceTest.php
+++ tests/Unit/HasNamespaceSetterTrait/Methods/SetNamespace/Parameters/NamespaceTest.php
@@ -18,4 +18,20 @@ class NamespaceTest extends TestCase
         $isExist = in_array($this->parameter , $parameters);
         $this->assertTrue($isExist, "method $this->method() in $this->namespace no have parameter with name $this->parameter");
     }
+    public function test_should_have_string_dataType():void {
+        $parameterReflection = new \ReflectionParameter([$this->namespace,$this->method],$this->parameter);
+        $expect = ['string'];
+        sort($expect,SORT_STRING);
+        $expect = implode('|',$expect);
+        if ($parameterReflection->hasType()) {
+            $types = (string) $parameterReflection->getType();
+            $types = explode('|',$types);
+            sort($types,SORT_STRING);
+            $types = implode('|',$types);
+            $this->assertEquals($expect,$types,"parameter $$this->parameter in $this->method() method from $this->namespace trait should have $expect dataType but is $types ");
+        }
+        else
+            $this->fail("parameter $$this->parameter in method $this->method() from $this->namespace trait should have any type !!! currently no type hinted ");
+    }
+
 }
