99b4a4a588b3b8f501dcf2a057df999c552fd2b0
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 04:35:11 +0200

    [file]{MethodsTest.php}=> define test method

--- tests/Unit/HasNamespaceSetterTrait/Methods/MethodsTest.php
+++ tests/Unit/HasNamespaceSetterTrait/Methods/MethodsTest.php
@@ -14,4 +14,23 @@ class MethodsTest extends TestCase
         $methodsCount = count($methods);
         $this->assertEquals(0,$methodsCount);
     }
+    public function test_no_have_static_method():void {
+        $traitReflection = new \ReflectionClass($this->namespace);
+        $methods = $traitReflection->getMethods();
+        $staticMethods = array_filter($methods,function (\ReflectionMethod $method){
+            return $method->isStatic();
+        });
+        $staticMethods[] = 'abol';
+        $staticMethods[] = 'abdol';
+        $staticMethodsCount = count($staticMethods);
+        $message = '';
+        if ($staticMethodsCount>0){
+            if ($staticMethodsCount==1)
+                $message = "method $staticMethods[0]() ";
+            else
+                $message = 'methods '.implode('() , ',$staticMethods).'() ';
+            $message .= "should dont be static in $this->namespace !!!";
+        }
+        $this->assertEmpty($staticMethods,$message);
+    }
 }
