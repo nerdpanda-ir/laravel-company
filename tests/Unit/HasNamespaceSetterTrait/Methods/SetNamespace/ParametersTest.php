e71cb91798c3f48c7bfbddc880784a580ad240cc
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 05:59:48 +0200

    [file]{ParametersTest.php}=> create test method

--- tests/Unit/HasNamespaceSetterTrait/Methods/SetNamespace/ParametersTest.php
+++ tests/Unit/HasNamespaceSetterTrait/Methods/SetNamespace/ParametersTest.php
@@ -9,5 +9,9 @@ class ParametersTest extends TestCase
 {
     protected string $namespace = HasNamespaceSetterTrait::class ;
     protected string $method = 'setNamespace';
-    
+    public function test_just_have_single_parameter():void {
+        $reflection = new \ReflectionMethod($this->namespace , $this->method);
+        $parametersCount = $reflection->getNumberOfParameters();
+        $this->assertEquals(1,$parametersCount,"method $this->method in $this->namespace should only one parameter");
+    }
 }
