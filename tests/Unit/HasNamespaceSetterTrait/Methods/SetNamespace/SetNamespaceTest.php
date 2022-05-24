a2b47c654fe3978cdfba4623855af1cbd3e557c6
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 05:36:06 +0200

    [file]{SetNamespaceTest.php}=> create test method

--- tests/Unit/HasNamespaceSetterTrait/Methods/SetNamespace/SetNamespaceTest.php
+++ tests/Unit/HasNamespaceSetterTrait/Methods/SetNamespace/SetNamespaceTest.php
@@ -9,4 +9,8 @@ class SetNamespaceTest extends TestCase
 {
     protected string $namespace = HasNamespaceSetterTrait::class ;
     protected string $method = 'setNamespace';
+    public function test_is_exist():void {
+        $isExist = method_exists($this->namespace , $this->method);
+        $this->assertTrue($isExist,"method $this->method() in $this->namespace is missing ");
+    }
 }
