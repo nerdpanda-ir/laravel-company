77c8592c30c8f5bb57dcb39e015b143155ce2522
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 06:06:03 +0200

    [file]{NamespaceTest.php}=> clean test class

--- tests/Unit/HasNamespaceSetterTrait/Methods/SetNamespace/Parameters/NamespaceTest.php
+++ tests/Unit/HasNamespaceSetterTrait/Methods/SetNamespace/Parameters/NamespaceTest.php
@@ -9,13 +9,5 @@ class NamespaceTest extends TestCase
     protected string $namespace = HasNamespaceSetterTrait::class ;
     protected string $method = 'setNamespace';
     protected string $parameter = 'namespace';
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $this->assertTrue(true);
-    }
+    
 }
