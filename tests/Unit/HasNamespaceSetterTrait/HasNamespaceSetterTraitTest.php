fc16e21f37483e80e84db15bbc97c18fa2229b05
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 03:55:55 +0200

    [file]{HasNamespaceSetterTraitTest}=> create test class

---
+++ tests/Unit/HasNamespaceSetterTrait/HasNamespaceSetterTraitTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\HasNamespaceSetterTrait;
+
+use PHPUnit\Framework\TestCase;
+
+class HasNamespaceSetterTraitTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
