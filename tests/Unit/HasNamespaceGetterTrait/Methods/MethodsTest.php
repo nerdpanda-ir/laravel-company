5101b30ba0316e0138af5a2804383d50f5f72d39
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 14:45:04 +0200

    [file]{MethodTest.php}=> refactor test method

--- tests/Unit/HasNamespaceGetterTrait/Methods/MethodsTest.php
+++ tests/Unit/HasNamespaceGetterTrait/Methods/MethodsTest.php
@@ -18,27 +18,31 @@ class MethodsTest extends TestCase
     public function test_should_have_public_methods():void{
         $traitReflection = new \ReflectionClass($this->namespace);
         $methods = $traitReflection->getMethods();
-        $nonePublicMethods = array_filter($methods , function (\ReflectionMethod $method) {
-            return !$method->isPublic();
-        });
-        $nonePublicMethods = array_map(function (\ReflectionMethod $method){
-            $modifier = '';
-            if ($method->isPrivate())
-                $modifier = 'private';
-            else
-                $modifier = 'protected';
-            return $method->name.'() is '.$modifier;
-        },$nonePublicMethods);
-        $nonePublicMethodsCount = count($nonePublicMethods);
-        $message = '';
-        if ($nonePublicMethodsCount>0){
-            if ($nonePublicMethodsCount==1)
-                $message = "method $nonePublicMethods[0]";
-            else
-                $message = "methods ".implode(' , ',$nonePublicMethods).' ';
-            $message.= "in $this->namespace should be public !!! ";
+        if (!empty($methods)){
+            $nonePublicMethods = array_filter($methods , function (\ReflectionMethod $method) {
+                return !$method->isPublic();
+            });
+            $nonePublicMethods = array_map(function (\ReflectionMethod $method){
+                $modifier = '';
+                if ($method->isPrivate())
+                    $modifier = 'private';
+                else
+                    $modifier = 'protected';
+                return $method->name.'() is '.$modifier;
+            },$nonePublicMethods);
+            $nonePublicMethodsCount = count($nonePublicMethods);
+            $message = '';
+            if ($nonePublicMethodsCount>0){
+                if ($nonePublicMethodsCount==1)
+                    $message = "method $nonePublicMethods[0]";
+                else
+                    $message = "methods ".implode(' , ',$nonePublicMethods).' ';
+                $message.= "in $this->namespace should be public !!! ";
+            }
+            $this->assertEquals(0,$nonePublicMethodsCount,$message);
         }
-        $this->assertEquals(0,$nonePublicMethodsCount,$message);
+        else
+            $this->fail("no found any method in $this->namespace trait !!!");
     }
     public function test_should_no_have_static_method():void {
         $traitReflection = new \ReflectionClass($this->namespace);
@@ -55,7 +59,8 @@ class MethodsTest extends TestCase
                 $message = 'methods '.implode('() , ',$staticMethods).'() , ';
             $message .= "dont be static !!! in $this->namespace trait ";
         }
-        $this->assertEquals(0,$staticMethodsCount,$message);
+        else
+            $this->fail("no found any method in $this->namespace trait !!!");
     }
     public function test_should_no_have_final_method():void {
         $traitReflection = new \ReflectionClass($this->namespace);
