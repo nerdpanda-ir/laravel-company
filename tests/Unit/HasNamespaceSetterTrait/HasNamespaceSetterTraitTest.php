eef5b958cdf235f9d10cd5a13f9ca007fdd23dbb
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 04:07:06 +0200

    [file]{HasNamespaceSetterTraitTest}=> create test method

--- tests/Unit/HasNamespaceSetterTrait/HasNamespaceSetterTraitTest.php
+++ tests/Unit/HasNamespaceSetterTrait/HasNamespaceSetterTraitTest.php
@@ -12,4 +12,8 @@ class HasNamespaceSetterTraitTest extends TestCase
         $isExist = trait_exists($this->namespace);
         $this->assertTrue($isExist,"trait $this->namespace should is exist !!!");
     }    
+    public function test_never_use_other_traits():void {
+        $uses = class_uses($this->namespace);
+        $this->assertEmpty($uses,"$this->namespace trait should no use other traits ");
+    }
 }
