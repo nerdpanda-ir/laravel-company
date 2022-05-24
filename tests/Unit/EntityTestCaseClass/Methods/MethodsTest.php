68d6d4e0ec83899bb3021aa13d723ef97a1e647d
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 10:29:41 +0200

    [file]{MethodsTest.php}=> create test class

---
+++ tests/Unit/EntityTestCaseClass/Methods/MethodsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\EntityTestCaseClass\Methods;
+
+use PHPUnit\Framework\TestCase;
+
+class MethodsTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
