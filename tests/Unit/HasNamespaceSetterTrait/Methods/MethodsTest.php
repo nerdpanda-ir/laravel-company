20b826ffbc7302de3ed340855df25b8497da1735
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 05:15:44 +0200

    [file]{MethodsTest.php}=> define test method and reposition test method

--- tests/Unit/HasNamespaceSetterTrait/Methods/MethodsTest.php
+++ tests/Unit/HasNamespaceSetterTrait/Methods/MethodsTest.php
@@ -14,30 +14,12 @@ class MethodsTest extends TestCase
         $methodsCount = count($methods);
         $this->assertEquals(0,$methodsCount);
     }
-    public function test_no_have_static_method():void {
-        $traitReflection = new \ReflectionClass($this->namespace);
-        $methods = $traitReflection->getMethods();
-        $staticMethods = array_filter($methods,function (\ReflectionMethod $method){
-            return $method->isStatic();
-        });
-        $staticMethods[] = 'abol';
-        $staticMethods[] = 'abdol';
-        $staticMethodsCount = count($staticMethods);
-        $message = '';
-        if ($staticMethodsCount>0){
-            if ($staticMethodsCount==1)
-                $message = "method $staticMethods[0]() ";
-            else
-                $message = 'methods '.implode('() , ',$staticMethods).'() ';
-            $message .= "should dont be static in $this->namespace !!!";
-        }
-        $this->assertEmpty($staticMethods,$message);
-    }
+    //@todo
     public function test_just_have_public_method():void {
         $traitReflection = new \ReflectionClass($this->namespace);
         $methods = $traitReflection->getMethods();
         $publicMethods = array_filter($methods,function (\ReflectionMethod $method){
-           return $method->isPublic();
+            return $method->isPublic();
         });
         $nonePublicMethods = array_map(function (\ReflectionMethod $method){
             $accessModifier = '';
@@ -58,5 +40,43 @@ class MethodsTest extends TestCase
             $message .=" in $this->namespace trait -> should be public !!!";
         }
         $this->assertEquals(0,$nonePublicMethodsCount,$message);
+    }
+    public function test_no_have_static_method():void {
+        $traitReflection = new \ReflectionClass($this->namespace);
+        $methods = $traitReflection->getMethods();
+        $staticMethods = array_filter($methods,function (\ReflectionMethod $method){
+            return $method->isStatic();
+        });
+        $staticMethods[] = 'abol';
+        $staticMethods[] = 'abdol';
+        $staticMethodsCount = count($staticMethods);
+        $message = '';
+        if ($staticMethodsCount>0){
+            if ($staticMethodsCount==1)
+                $message = "method $staticMethods[0]() ";
+            else
+                $message = 'methods '.implode('() , ',$staticMethods).'() ';
+            $message .= "should dont be static in $this->namespace !!!";
+        }
+        $this->assertEmpty($staticMethods,$message);
+    }
+    public function test_no_have_final_method():void {
+        $traitReflection = new \ReflectionClass($this->namespace);
+        $methods = $traitReflection->getMethods();
+        $finalMethods = array_filter($methods,function (\ReflectionMethod $method){
+            return $method->isFinal();
+        });
+        $finalMethods[] = 'ez';
+        $finalMethods[] = 'gg';
+        $finalMethodsCount = count($finalMethods);
+        $message = '';
+        if ($finalMethodsCount>0) {
+            if ($finalMethodsCount==1)
+                $message = "method $finalMethods[0]() ";
+            else
+                $message = 'method'.implode('() , ',$finalMethods).'() ';
+            $message.=" in $this->namespace trait should dont be final !!! ";
+        }
+        $this->assertEmpty($finalMethods,$message);
     }
 }
