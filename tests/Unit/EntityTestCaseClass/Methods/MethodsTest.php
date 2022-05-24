4bf7db7534cac70d092697f526449185a3cbd405
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 10:30:51 +0200

    [file]{MethodsTest.php}=> use class namespace

--- tests/Unit/EntityTestCaseClass/Methods/MethodsTest.php
+++ tests/Unit/EntityTestCaseClass/Methods/MethodsTest.php
@@ -3,16 +3,9 @@
 namespace Tests\Unit\EntityTestCaseClass\Methods;
 
 use PHPUnit\Framework\TestCase;
+use Tests\EntityTestCase;
 
 class MethodsTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $this->assertTrue(true);
-    }
+    
 }
