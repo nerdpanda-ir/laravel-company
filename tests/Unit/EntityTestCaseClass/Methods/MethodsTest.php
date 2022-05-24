dbe9f8e5440c9710d767b9999551758397a6c2f5
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 14:04:50 +0200

    [file]{MethodsTest.php}=> create test methods

--- tests/Unit/EntityTestCaseClass/Methods/MethodsTest.php
+++ tests/Unit/EntityTestCaseClass/Methods/MethodsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\EntityTestCaseClass\Methods;
 
 use App\Traits\HasNamespaceGetterTrait;
+use App\Traits\HasNamespaceSetterTrait;
 use PHPUnit\Framework\TestCase;
 use Tests\EntityTestCase;
 
@@ -151,5 +152,120 @@ class MethodsTest extends TestCase
         }
         else
             $this->fail(" no found any method in used trait ".HasNamespaceGetterTrait::class ." in $this->namespace class !!!");
+    }
+    public function test_should_have_one_method_from_HasNamespaceSetterTrait():void {
+        $classReflection = new \ReflectionClass($this->namespace);
+        $methods = $classReflection->getMethods();
+        $counter = 0 ;
+        foreach ($methods as $method)
+            if ($method->class==HasNamespaceSetterTrait::class)
+                $counter++;
+        $this->assertEquals(1,$counter,"$this->namespace class should just have one method from trait ".HasNamespaceSetterTrait::class);
+    }
+    public function test_methods_from_HasNamespaceSetterTrait_should_is_public(){
+        $reflectionClass = new \ReflectionClass($this->namespace);
+        $methods = $reflectionClass->getMethods();
+        $traitMethods = array_filter($methods,function (\ReflectionMethod $method){
+            return $method->class==HasNamespaceSetterTrait::class;
+        });
+        if (!empty($traitMethods)){
+            $nonePublicTraitMethods = array_filter($traitMethods,function (\ReflectionMethod $method){
+                return !$method->isPublic();
+            });
+            $nonePublicTraitMethods = array_map(function (\ReflectionMethod $method) {
+                return $method->name.'()';
+            },$nonePublicTraitMethods);
+            $nonePublicTraitMethodsCount = count($nonePublicTraitMethods);
+            $message = '';
+            if (!empty($nonePublicTraitMethods)){
+                if ($nonePublicTraitMethodsCount==1)
+                    $message = " method $nonePublicTraitMethods[0] ";
+                else
+                    $message = 'methods '.implode(', ',$nonePublicTraitMethods);
+                $message .= ' in trait '.HasNamespaceSetterTrait::class .'should is public !!!';
+            }
+            $this->assertEmpty($nonePublicTraitMethods,$message);
+        }
+        else
+            $this->fail("no found any methods in use trait ".HasNamespaceSetterTrait::class." in class $this->namespace");
+    }
+    public function test_methods_from_HasNamespaceSetter_should_is_no_static():void {
+        $classReflection = new \ReflectionClass($this->namespace);
+        $methods = $classReflection->getMethods();
+        $traitMethods = array_filter($methods,function (\ReflectionMethod $method){
+            return $method->class == HasNamespaceSetterTrait::class;
+        });
+        if (!empty($traitMethods)){
+            $staticMethods = array_filter($traitMethods , function (\ReflectionMethod $method){
+                return $method->isStatic();
+            });
+            $staticMethods = array_map(function (\ReflectionMethod $method){
+                return $method->name.'() ';
+            },$staticMethods);
+            $staticCount = count($staticMethods);
+            $message = '';
+            if ($staticCount>0){
+                if ($staticCount==1)
+                    $message ="method $staticMethods[0]";
+                else
+                    $message ="methods ".implode(', ',$staticMethods);
+                $message.="in used trait ".HasNamespaceSetterTrait::class." in $this->namespace class dont be static ";
+            }
+            $this->assertEmpty($staticMethods,$message);
+        }
+        else
+            $this->fail("no found any methods in use trait ".HasNamespaceSetterTrait::class . " in class $this->namespace ");
+    }
+    public function test_methods_in_HasNamespaceSetterTrait_should_no_final():void {
+        $classReflection = new \ReflectionClass($this->namespace);
+        $methods = $classReflection->getMethods();
+        $traitMethods = array_filter($methods,function (\ReflectionMethod $method){
+            return $method->class==HasNamespaceSetterTrait::class;
+        });
+        if (!empty($traitMethods)){
+            $finalMethods = array_filter($traitMethods,function (\ReflectionMethod $method){
+                return $method->isFinal();
+            });
+            $message = '';
+            if (!empty($finalMethods)){
+                $finalMethods = array_map(function (\ReflectionMethod $method){
+                    return $method->name.'()';
+                },$finalMethods);
+                $finalMethodsCount = count($finalMethods);
+                if ($finalMethodsCount==1)
+                    $message = "method $finalMethods[0]";
+                else
+                    $message = "methods ".implode(' , ',$finalMethods).' ';
+                $message .="in used trait".HasNamespaceSetterTrait::class . " in $this->namespace class dont be final !!!";
+            }
+            $this->assertEmpty($finalMethods,$message);
+        }
+        else
+            $this->fail("no found any method in use trait ".HasNamespaceSetterTrait::class . " in class $this->namespace ");
+    }
+    public function test_methods_in_HasNamespaceSetterTrait_should_no_abstract():void {
+        $reflectionClass = new \ReflectionClass($this->namespace);
+        $methods = $reflectionClass->getMethods();
+        $traitMethods = array_filter($methods , function (\ReflectionMethod $method){
+            return $method->class==HasNamespaceSetterTrait::class ;
+        });
+        if (!empty($traitMethods)){
+            $abstractMethods = [] ;
+            foreach ($traitMethods as $traitMethod)
+                if ($traitMethod->isAbstract())
+                    $abstractMethods[] = $traitMethod->name.'()';
+            $message = '';
+            if (!empty($abstractMethods)){
+                $abstractMethodsCount = count($abstractMethods);
+                if ($abstractMethodsCount==1)
+                    $message = "method $abstractMethods[0] " ;
+                else
+                    $message = "methods ".implode(' , ',$abstractMethods) ;
+                $message .= "in used trait".HasNamespaceSetterTrait::class . " in $this->namespace class dont be abstract !!!";
+            }
+            $this->assertEmpty($abstractMethods,$message);
+        }
+        else
+            $this->fail(" no found any method in used trait ".HasNamespaceSetterTrait::class ." in $this->namespace class !!!");
     }
 }
