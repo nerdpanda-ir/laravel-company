7b573f047ebb566f076009383e52933f36b53a26
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-24 06:19:13 +0200

    [file]{NamespaceTest.php}=> create test method

--- tests/Unit/HasNamespaceSetterTrait/Methods/SetNamespace/Parameters/NamespaceTest.php
+++ tests/Unit/HasNamespaceSetterTrait/Methods/SetNamespace/Parameters/NamespaceTest.php
@@ -9,5 +9,13 @@ class NamespaceTest extends TestCase
     protected string $namespace = HasNamespaceSetterTrait::class ;
     protected string $method = 'setNamespace';
     protected string $parameter = 'namespace';
-    
+    public function test_is_exist():void {
+        $methodReflection = new \ReflectionMethod($this->namespace,$this->method);
+        $parameters = $methodReflection->getParameters();
+        $parameters = array_map(function (\ReflectionParameter $parameter){
+            return $parameter->name;
+        },$parameters);
+        $isExist = in_array($this->parameter , $parameters);
+        $this->assertTrue($isExist, "method $this->method() in $this->namespace no have parameter with name $this->parameter");
+    }
 }
