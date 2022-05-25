b72105da39685641d08494acae1729710c07593e
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-25 05:53:28 +0200

    [file]{EntityTestCase.php}=> use trait

--- tests/EntityTestCase.php
+++ tests/EntityTestCase.php
@@ -1,5 +1,10 @@
-<?php namespace Tests ; ?>
+<?php namespace Tests ;
+
+use App\Traits\HasNamespaceGetterTrait;
+
+ ?>
 <?php
 class EntityTestCase extends TestCase {
+    use HasNamespaceGetterTrait;
     protected string $namespace;
 }
