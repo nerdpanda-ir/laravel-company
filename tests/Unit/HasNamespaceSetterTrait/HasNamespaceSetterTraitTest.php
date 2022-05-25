6aca3e3981c72b8bf49e83607a317532f369fbef
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-25 04:40:53 +0200

    [file]{HasNamespaceSetterTraitTest.php}=> modify value for property

--- tests/Unit/HasNamespaceSetterTrait/HasNamespaceSetterTraitTest.php
+++ tests/Unit/HasNamespaceSetterTrait/HasNamespaceSetterTraitTest.php
@@ -8,7 +8,7 @@ use App\Traits\HasNamespaceSetterTrait ;
 class HasNamespaceSetterTraitTest extends TestCase
 {
     //@todo can test property ?
-    protected string $namespace = HasNamespaceSetterTraitTest::class ;
+    protected string $namespace = HasNamespaceSetterTrait::class ;
     public function test_is_exist():void {
         $isExist = trait_exists($this->namespace);
         $this->assertTrue($isExist,"trait $this->namespace should is exist !!!");
