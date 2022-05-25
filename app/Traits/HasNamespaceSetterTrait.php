33b77e882368a10a4d281aa11413414546da3455
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-25 04:43:56 +0200

    [file]{HasNamespaceSetterTrait.php}=> change return type and remove return

--- app/Traits/HasNamespaceSetterTrait.php
+++ app/Traits/HasNamespaceSetterTrait.php
@@ -1,7 +1,7 @@
 <?php namespace App\Traits ;?>
 <?php
 trait HasNamespaceSetterTrait {
-    public function setNamespace():int{
-        return 1;
+    public function setNamespace():void{
+        
     }
 }
