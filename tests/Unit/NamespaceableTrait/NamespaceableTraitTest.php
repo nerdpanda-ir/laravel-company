23982981cf2b4b29325c9fe01d59d503a98db8f5
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-25 09:46:19 +0200

    [file]{NamespaceableTraitTest.php}=> define property

--- tests/Unit/NamespaceableTrait/NamespaceableTraitTest.php
+++ tests/Unit/NamespaceableTrait/NamespaceableTraitTest.php
@@ -4,7 +4,9 @@ namespace Tests\Unit\NamespaceableTrait;
 
 use PHPUnit\Framework\TestCase;
 use Tests\Unit\NamespaceableTrait;
+
 class NamespaceableTraitTest extends TestCase
 {
-
+    protected string $namespace = NamespaceableTrait::class ;
+    
 }
