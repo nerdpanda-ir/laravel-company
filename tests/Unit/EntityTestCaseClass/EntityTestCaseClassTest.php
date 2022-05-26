2e8d4afe5a3f29b26c1785666f36acd61b4e4c99
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-26 05:07:08 +0200

    [file]{EntityTestCaseClassTest.php}=> clean test class

--- tests/Unit/EntityTestCaseClass/EntityTestCaseClassTest.php
+++ tests/Unit/EntityTestCaseClass/EntityTestCaseClassTest.php
@@ -6,13 +6,5 @@ use PHPUnit\Framework\TestCase;
 
 class EntityTestCaseClassTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $this->assertTrue(true);
-    }
+    
 }
