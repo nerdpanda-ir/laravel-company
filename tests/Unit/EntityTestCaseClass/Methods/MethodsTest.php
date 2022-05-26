82f234881c0cad50f192c77823cc54b67d64d571
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-26 06:11:03 +0200

    [file]{MethodsTest.php}=> create test method

--- tests/Unit/EntityTestCaseClass/Methods/MethodsTest.php
+++ tests/Unit/EntityTestCaseClass/Methods/MethodsTest.php
@@ -8,4 +8,12 @@ use Tests\Unit\EntityTestCase;
 class MethodsTest extends TestCase
 {
     protected string $namespace = EntityTestCase::class ;
+    public function test_should_no_have_any_method_in_self():void {
+        $reflection = new \ReflectionClass($this->namespace);
+        $methods = $reflection->getMethods();
+        $methods = array_filter($methods,function (\ReflectionMethod $method){
+            return $method->class==$this->namespace;
+        });
+        $this->assertEmpty($methods," should no have any method in $this->namespace class !!!");
+    }
 }
