4e7d711757ba019c907cc5bd35a490a073f4216a
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-26 05:11:58 +0200

    [file]{EntityTestCaseClassTest.php}=> create test method

--- tests/Unit/EntityTestCaseClass/EntityTestCaseClassTest.php
+++ tests/Unit/EntityTestCaseClass/EntityTestCaseClassTest.php
@@ -8,5 +8,9 @@ use Tests\EntityTestCase;
 class EntityTestCaseClassTest extends TestCase
 {
     protected string $namespace = EntityTestCase::class ;
-    
+    public function test_is_exist():void {
+        $isExist = class_exists($this->namespace);
+        $this->assertTrue($isExist," missing class $this->namespace
+         ");
+    }
 }
