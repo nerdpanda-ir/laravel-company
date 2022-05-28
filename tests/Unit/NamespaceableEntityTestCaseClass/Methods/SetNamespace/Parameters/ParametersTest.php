60bffaaeb07a0db4a89537dc90e0daa1ef5ccda8
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-28 12:10:56 +0200

    [file]{ParametersTest.php}=> use class namespace

--- tests/Unit/NamespaceableEntityTestCaseClass/Methods/SetNamespace/Parameters/ParametersTest.php
+++ tests/Unit/NamespaceableEntityTestCaseClass/Methods/SetNamespace/Parameters/ParametersTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Unit\NamespaceableEntityTestCaseClass\Methods\SetNamespace\Parameters;
 
 use PHPUnit\Framework\TestCase;
-
+use Tests\NamespaceableEntityTestCase;
 
 class ParametersTest extends TestCase
 {
