f9a862e6aa8bc3dfcdefc6e7f03e8ded31d2e332
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-28 07:45:55 +0200

    [file]{NamespaceableEntityTestCaseTraitTest.php}=> create test method

--- tests/Unit/NamespaceableEntityTestCaseTrait/NamespaceableEntityTestCaseTraitTest.php
+++ tests/Unit/NamespaceableEntityTestCaseTrait/NamespaceableEntityTestCaseTraitTest.php
@@ -8,4 +8,8 @@ use App\Traits\Tests\NamespaceableEntityTestCaseTrait;
 class NamespaceableEntityTestCaseTraitTest extends TestCase
 {
     protected string $namespace = NamespaceableEntityTestCaseTrait::class ;
+    public function test_is_exist():void {
+        $isExist = trait_exists($this->namespace);
+        $this->assertTrue($isExist,"missing $this->namespace trait !!!");
+    }
 }
