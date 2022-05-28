8fc9e681075690836a563bbde1dedfa901a20058
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-28 07:58:51 +0200

    [file]{NamespaceableEntityTestCaseTraitTest.php}=> create test method

--- tests/Unit/NamespaceableEntityTestCaseTrait/NamespaceableEntityTestCaseTraitTest.php
+++ tests/Unit/NamespaceableEntityTestCaseTrait/NamespaceableEntityTestCaseTraitTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\NamespaceableEntityTestCaseTrait;
 
+use App\Traits\NamespaceableTrait;
 use PHPUnit\Framework\TestCase;
 use App\Traits\Tests\NamespaceableEntityTestCaseTrait;
 
@@ -16,5 +17,11 @@ class NamespaceableEntityTestCaseTraitTest extends TestCase
         $uses = class_uses($this->namespace);
         $expect = 1 ;
         $this->assertCount($expect, $uses,"$this->namespace trait should use one trait !!!");
+    }
+    public function test_should_use_namespaceableTrait():void {
+        $uses = class_uses($this->namespace);
+        $expect = NamespaceableTrait::class;
+        $isUse = in_array($expect , $uses);
+        $this->assertTrue($isUse,"$this->namespace trait should use $expect trait ");
     }
 }
