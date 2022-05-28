a8472e92f92a4f200f9ce2dc0d338c83c99fbbf9
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-28 07:29:54 +0200

    [file]{ParametersTest.php}=> modify value for namespace property

--- tests/Unit/NamespaceableEntityTestCaseInterface/Methods/Namespace/ParametersTest.php
+++ tests/Unit/NamespaceableEntityTestCaseInterface/Methods/Namespace/ParametersTest.php
@@ -7,7 +7,7 @@ use App\Contracts\Tests\NamespaceableEntityTestCaseInterface;
 
 class ParametersTest extends TestCase
 {
-    protected string $namespace = EntityTestCaseInterface::class ;
+    protected string $namespace = NamespaceableEntityTestCaseInterface::class ;
     protected string $method = 'namespace';
     public function test_never_exist_parameter():void {
         $methodReflection = new \ReflectionMethod($this->namespace,$this->method);
