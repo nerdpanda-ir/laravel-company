aba7bb0d2b9a90188e6624a44c3f3234632ed5e6
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-28 07:36:25 +0200

    [file]{NamespaceableEntityTestCaseTraitTest.php}=> create test class

---
+++ tests/Unit/NamespaceableEntityTestCaseTrait/NamespaceableEntityTestCaseTraitTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\NamespaceableEntityTestCaseTrait;
+
+use PHPUnit\Framework\TestCase;
+
+class NamespaceableEntityTestCaseTraitTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
