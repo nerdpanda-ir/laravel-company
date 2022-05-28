09da8874043dfcd9e6cea5befb3a00adf433f009
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-28 10:37:11 +0200

    [file]{Properties.php}=> create test class

---
+++ tests/Unit/HasNamespaceGetterTrait/Properties/PropertiesTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\HasNamespaceGetterTrait\Properties;
+
+use PHPUnit\Framework\TestCase;
+
+class PropertiesTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
