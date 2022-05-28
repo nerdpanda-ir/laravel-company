588145205c7c91add3a847d5f63cd45bbbffea80
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-28 08:39:58 +0200

    [file]{ParametersTest.php}=> create test class for test parameters

---
+++ tests/Unit/NamespaceableEntityTestCaseTrait/Methods/SetNamespace/Parameters/ParametersTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\NamespaceableTrait\Methods\SetNamespace\Parameters;
+
+use App\Traits\NamespaceableTrait;
+use App\Traits\Tests\NamespaceableEntityTestCaseTrait;
+use PHPUnit\Framework\TestCase;
+
+class ParametersTest extends TestCase
+{
+    protected string $namespace = NamespaceableEntityTestCaseTrait::class ;
+    protected string $method = 'setNamespace';
+    public function test_just_have_single_parameter():void {
+        $reflection = new \ReflectionMethod($this->namespace , $this->method);
+        $parametersCount = $reflection->getNumberOfParameters();
+        $this->assertEquals(1,$parametersCount,"method $this->method in $this->namespace trait should only one parameter");
+    }
+}
