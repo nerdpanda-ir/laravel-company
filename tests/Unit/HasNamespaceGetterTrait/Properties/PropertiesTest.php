d45776cc96fdad75095424153e912fd554b86c99
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-28 10:49:49 +0200

    [file]{Properties.php}=> create tese method

--- tests/Unit/HasNamespaceGetterTrait/Properties/PropertiesTest.php
+++ tests/Unit/HasNamespaceGetterTrait/Properties/PropertiesTest.php
@@ -8,5 +8,9 @@ use App\Traits\HasNamespaceGetterTrait;
 class PropertiesTest extends TestCase
 {
     protected string $namespace = HasNamespaceGetterTrait::class ;
-    
+    public function test_no_have_property():void {
+        $reflection = new \ReflectionClass($this->namespace);
+        $properties = $reflection->getProperties();
+        $this->assertEmpty($properties,"$this->namespace  should no have any property ");
+    }
 }
