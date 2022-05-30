d856a2a76aaeaeeac1e4ba51973a69958e8ce3da
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 07:16:44 +0200

    [file]{BlogPageApiController.php}=> implement interface

--- app/Http/Controllers/Api/V1/BlogPageApiController.php
+++ app/Http/Controllers/Api/V1/BlogPageApiController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Contracts\BlogPageApiControllerInterface;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\Pages\BlogPageResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class BlogPageApiController extends Controller
+class BlogPageApiController extends Controller implements BlogPageApiControllerInterface
 {
     public function __invoke():JsonResource {
         $data = [
