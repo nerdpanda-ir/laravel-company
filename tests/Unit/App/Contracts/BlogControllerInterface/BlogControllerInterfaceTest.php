374dec8a44c306252d70620a1d9a0bed04cf683e
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 06:12:23 +0200

    [dir]{BlogControllerInterface}=> create test classes for blog controller interface

---
+++ tests/Unit/App/Contracts/BlogControllerInterface/BlogControllerInterfaceTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit\App\Contracts\BlogControllerInterface;
+
+use PHPUnit\Framework\TestCase;
+use App\Contracts\BlogControllerInterface;
+
+class BlogControllerInterfaceTest extends TestCase
+{
+    protected string $namespace = BlogControllerInterface::class ;
+    public function test_is_exist():void {
+        $isExist = interface_exists($this->namespace);
+        $this->assertTrue($isExist,"missing $this->namespace interface !!");
+    }
+    public function test_no_extend_interface():void {
+        $extends = class_implements($this->namespace);
+        $this->assertEmpty($extends,"$this->namespace interface should is never extend any interface ");
+    }
+}
