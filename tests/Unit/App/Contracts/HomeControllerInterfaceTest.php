2c03e655c11a237628c85cb84e84f8574de74764
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 04:36:24 +0200

    [file]{HomeControllerInterfaceTest.php.php}=> define test method

--- tests/Unit/App/Contracts/HomeControllerInterfaceTest.php
+++ tests/Unit/App/Contracts/HomeControllerInterfaceTest.php
@@ -8,5 +8,8 @@ use App\Contracts\HomeControllerInterface;
 class HomeControllerInterfaceTest extends TestCase
 {
     protected string $namespace = HomeControllerInterface::class ;
-    
+    public function test_is_exist():void {
+        $isExist = interface_exists($this->namespace);
+        $this->assertTrue($isExist,"missing $this->namespace interface !!");
+    }
 }
