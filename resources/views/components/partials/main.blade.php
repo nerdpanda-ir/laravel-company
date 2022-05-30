2e67f61b4070583001798c3726329acf36ef1686
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 12:18:20 +0200

    [file]{main.blade.php}=> add code to component

--- resources/views/components/partials/main.blade.php
+++ resources/views/components/partials/main.blade.php
@@ -1,14 +1,18 @@
 @props(['title','describe'])
-<x-partials.bottom-header-area />
+<x-partials.bottom-header-area/>
 <div class="blog-page area-padding">
     <div class="container">
         <div class="row">
-            <x-partials.aside />
+            <x-partials.aside/>
             <!-- Start single blog -->
             <div class="col-md-8 col-sm-8 col-xs-12">
-                {{ $slot }}
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        {{ $slot }}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
-<x-clear-fix />
+<x-clear-fix/>
