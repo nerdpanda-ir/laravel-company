cfcb43195897dac6f1974da15953fe169b141bc0
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 05:29:40 +0200

    [file]{MethodsTest.php}=> define property

--- tests/Unit/App/Contracts/HomeControllerInterface/Methods/MethodsTest.php
+++ tests/Unit/App/Contracts/HomeControllerInterface/Methods/MethodsTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Unit\App\Contracts\HomeControllerInterface\Methods;
 
+use App\Contracts\HomeControllerInterface;
 use PHPUnit\Framework\TestCase;
 
 class MethodsTest extends TestCase
 {
-
+    protected string $namespace = HomeControllerInterface::class ;
+    
 }
