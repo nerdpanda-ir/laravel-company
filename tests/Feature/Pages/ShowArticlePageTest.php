f176bd289c38ddd1cff16ce3aec8f2410ad94ec3
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 08:53:37 +0200

    [file]{ShowArticlePageTest.php}=> create test class

---
+++ tests/Feature/Pages/ShowArticlePageTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Pages;
+
+use Tests\TestCase;
+
+class ShowArticlePageTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
