8c7bd8a8ad11d9a07084d9ce34ad2110f9b1ca67
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 08:28:19 +0200

    [file]{ShowArticleController.php}=> implement interface

--- app/Http/Controllers/ShowArticleController.php
+++ app/Http/Controllers/ShowArticleController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contracts\ShowArticleControllerInterface;
 
-class ShowArticleController extends Controller
+class ShowArticleController extends Controller implements ShowArticleControllerInterface
 {
     //
 }
