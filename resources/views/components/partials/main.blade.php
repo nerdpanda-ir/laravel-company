588c9dab38cd4d25f6bf450ee0d216009d7fa479
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 11:54:05 +0200

    [file]{main.blade.php}=> define props and craete components codes !!

--- resources/views/components/partials/main.blade.php
+++ resources/views/components/partials/main.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+@props(['title','describe'])
+<x-partials.bottom-header-area />
+
+<div class="blog-page area-padding">
+    <div class="container">
+        <div class="row">
+            <x-partials.aside />
+            <!-- Start single blog -->
+            <div class="col-md-8 col-sm-8 col-xs-12">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
 </div>
