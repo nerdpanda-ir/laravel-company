df76a71895f8f26daf9b5381667111ba9d590101
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 11:25:19 +0200

    [file]{main.blade.php}=> modify used component path and pass props to component !!!

--- resources/views/components/partials/pages/blog/main.blade.php
+++ resources/views/components/partials/pages/blog/main.blade.php
@@ -1,3 +1,3 @@
-<x-partials.pages.blog.bottom-header-area />
+<x-partials.bottom-header-area title="all posts " describe="this is all posts page !!" />
 <x-partials.pages.blog.blog-area />
 <div class="clearfix"></div>
