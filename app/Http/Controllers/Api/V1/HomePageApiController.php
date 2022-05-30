c3f5c8acbc44c619e9a28a8c9288a1934caeaaec
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 06:57:49 +0200

    [file]{HomePageApiController.php} => implement interface

--- app/Http/Controllers/Api/V1/HomePageApiController.php
+++ app/Http/Controllers/Api/V1/HomePageApiController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Contracts\HomePageApiControllerInterface;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\Api\V1\Pages\HomePageResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class HomePageApiController extends Controller
+class HomePageApiController extends Controller implements HomePageApiControllerInterface
 {
     public function __invoke() :JsonResource {
         $data = [
