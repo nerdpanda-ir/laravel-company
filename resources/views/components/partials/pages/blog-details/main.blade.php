9b8745c583c816255ec816a0f0fc15b90a171ec6
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 11:27:47 +0200

    [file]{main.blade.php}=> use  component

--- resources/views/components/partials/pages/blog-details/main.blade.php
+++ resources/views/components/partials/pages/blog-details/main.blade.php
@@ -1,24 +1,4 @@
-<!-- Start Bottom Header -->
-<div class="header-bg page-area">
-    <div class="home-overly"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="slider-content text-center">
-                    <div class="header-bottom">
-                        <div class="layer2 wow zoomIn" data-wow-duration="1s" data-wow-delay=".4s">
-                            <h1 class="title2">Blog Details </h1>
-                        </div>
-                        <div class="layer3 wow zoomInUp" data-wow-duration="2s" data-wow-delay="1s">
-                            <h2 class="title3">profesional Blog Page</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- END Header -->
+<x-partials.bottom-header-area title="single post " describe="show post (post details)"/>
 <div class="blog-page area-padding">
     <div class="container">
         <div class="row">
