d9ece36b85b3013cba9aace522115b2f0df613b0
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 04:55:24 +0200

    [file]{ConstantsTest.php}=> define property

--- tests/Unit/App/Contracts/HomeControllerInterface/Constants/ConstantsTest.php
+++ tests/Unit/App/Contracts/HomeControllerInterface/Constants/ConstantsTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Unit\App\Contracts\HomeControllerInterface\Constants;
 
+use App\Contracts\HomeControllerInterface;
 use PHPUnit\Framework\TestCase;
 
 class ConstantsTest extends TestCase
 {
+    protected string $namespace = HomeControllerInterface::class ;
     
 }
