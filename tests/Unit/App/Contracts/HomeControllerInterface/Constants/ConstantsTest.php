2cf235bf3acaea651e1038187952bccf35da2a24
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 05:25:56 +0200

    [file]{ConstantsTest.php}=> define test method

--- tests/Unit/App/Contracts/HomeControllerInterface/Constants/ConstantsTest.php
+++ tests/Unit/App/Contracts/HomeControllerInterface/Constants/ConstantsTest.php
@@ -8,5 +8,28 @@ use PHPUnit\Framework\TestCase;
 class ConstantsTest extends TestCase
 {
     protected string $namespace = HomeControllerInterface::class ;
-    
+    public function test_no_have_constants():void {
+        $reflection = new \ReflectionClass($this->namespace);
+        $constants = $reflection->getReflectionConstants();
+        $constants = array_filter($constants , function (\ReflectionClassConstant $constant){
+            return $constant->getDeclaringClass()->name==$this->namespace;
+        });
+        $constantsCount = count($constants);
+        $expect = 0 ;
+        $message = '';
+        if ($constantsCount!=$expect){
+            $message .= "constraints in $this->namespace Interface ";
+            if ($constantsCount>0)
+                $constants = array_map(
+                    function (\ReflectionClassConstant $constant){
+                        return $constant->name;
+                    }, $constants);
+            if ($constantsCount>$expect)
+                $message.="more than expected";
+            else
+                $message.="less than expected";
+            $message.="\nexpect interface $this->namespace have $expect constants but have $constantsCount constants ".(($constantsCount>0) ? ' : ': '').implode(' , ',$constants)." !!!";
+        }
+        $this->assertEquals($expect,$constantsCount,$message);
+    }
 }
