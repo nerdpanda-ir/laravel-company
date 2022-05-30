81c25c6c40a7c4ed99e3ac6fc259aa48658ae41e
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 08:31:39 +0200

    [file]{ShowArticleController.php}=> create invoke method

--- app/Http/Controllers/ShowArticleController.php
+++ app/Http/Controllers/ShowArticleController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use App\Contracts\ShowArticleControllerInterface;
 
 class ShowArticleController extends Controller implements ShowArticleControllerInterface
 {
-    //
+    public function __invoke():View {
+        return \view('blog-details');
+    }
 }
