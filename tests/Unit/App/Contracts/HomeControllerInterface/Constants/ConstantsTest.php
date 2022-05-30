5f281cd132d034fbee5217ce6e3c9b9b16feb2ca
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 04:54:15 +0200

    [file]{ConstantsTest.php}=> create test class

---
+++ tests/Unit/App/Contracts/HomeControllerInterface/Constants/ConstantsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\App\Contracts\HomeControllerInterface\Constants;
+
+use PHPUnit\Framework\TestCase;
+
+class ConstantsTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
