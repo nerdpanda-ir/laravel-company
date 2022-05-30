9a6983945926070199a0cd87bae25da3aed00f5f
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 04:32:05 +0200

    [file]{HomeControllerInterfaceTest.php.php}=> create test class for home controller interface !!!

---
+++ tests/Unit/App/Contracts/HomeControllerInterfaceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\App\Contracts;
+
+use PHPUnit\Framework\TestCase;
+
+class HomeControllerInterfaceTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
