a33dd5a797084248a4db65795c5372eb6a12c4f5
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 05:43:26 +0200

    [file]{MethodsTest.php}=> define test method

--- tests/Unit/App/Contracts/HomeControllerInterface/Methods/MethodsTest.php
+++ tests/Unit/App/Contracts/HomeControllerInterface/Methods/MethodsTest.php
@@ -8,5 +8,25 @@ use PHPUnit\Framework\TestCase;
 class MethodsTest extends TestCase
 {
     protected string $namespace = HomeControllerInterface::class ;
-    
+    public function test_no_have_method():void {
+        $reflection = new \ReflectionClass($this->namespace);
+        $methods = $reflection->getMethods() ;
+        $methods = array_filter(
+            $methods ,
+            function (\ReflectionMethod $method){
+                return $method->getDeclaringClass()->name == $this->namespace;
+            }
+        );
+        $message = '';
+        if (!empty($methods)){
+            $message .= "interface $this->namespace should no have any method ";
+            $methods = array_map(
+                function (\ReflectionMethod $method){
+                    return $method->name;
+                } , $methods );
+            $message.="but have ".implode('() , ',$methods).'()';
+        }
+        $this->assertEmpty($methods,$message);
+
+    }
 }
