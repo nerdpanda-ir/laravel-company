cc3ac6e8deb66d5bb047fcdbbf6af04769e0d86c
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 13:06:37 +0200

    [file]{blog-area.blade.php}=> modify cods

--- resources/views/components/partials/pages/blog/blog-area.blade.php
+++ resources/views/components/partials/pages/blog/blog-area.blade.php
@@ -1,173 +1,163 @@
-<!-- End Blog Area -->
-<div class="blog-page area-padding">
-    <div class="container">
-        <div class="row">
-
-            <!-- Start single blog -->
-            <div class="col-md-8 col-sm-8 col-xs-12">
-                <div class="row">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <div class="single-blog">
-                            <div class="single-blog-img">
-                                <a href="blog-details.blade.php">
-                                    <img src="img/blog/1.jpg" alt="">
-                                </a>
-                            </div>
-                            <div class="blog-meta">
+<div class="single-blog">
+    <div class="single-blog-img">
+        <a href="blog-details.blade.php">
+            <img src="img/blog/1.jpg" alt="">
+        </a>
+    </div>
+    <div class="blog-meta">
                   <span class="comments-type">
 											<i class="fa fa-comment-o"></i>
 											<a href="#">11 comments</a>
 										</span>
-                                <span class="date-type">
+        <span class="date-type">
 											<i class="fa fa-calendar"></i>2016-03-05 / 09:10:16
 										</span>
-                            </div>
-                            <div class="blog-text">
-                                <h4>
-                                    <a href="#">Post my imagine Items</a>
-                                </h4>
-                                <p>
-                                    Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit.
-                                </p>
-                            </div>
-                            <span>
+    </div>
+    <div class="blog-text">
+        <h4>
+            <a href="#">Post my imagine Items</a>
+        </h4>
+        <p>
+            Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec
+            sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit.
+        </p>
+    </div>
+    <span>
 										<a href="blog-details.blade.php" class="ready-btn">Read more</a>
 									</span>
-                        </div>
-                    </div>
-                    <!-- End single blog -->
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <div class="single-blog">
-                            <div class="single-blog-img">
-                                <a href="blog-details.blade.php">
-                                    <img src="img/blog/2.jpg" alt="">
-                                </a>
-                            </div>
-                            <div class="blog-meta">
+</div>
+
+<!-- End single blog -->
+<div class="col-md-12 col-sm-12 col-xs-12">
+    <div class="single-blog">
+        <div class="single-blog-img">
+            <a href="blog-details.blade.php">
+                <img src="img/blog/2.jpg" alt="">
+            </a>
+        </div>
+        <div class="blog-meta">
                   <span class="comments-type">
 											<i class="fa fa-comment-o"></i>
 											<a href="#">7 comments</a>
 										</span>
-                                <span class="date-type">
+            <span class="date-type">
 											<i class="fa fa-calendar"></i>2016-03-05 / 09:10:16
 										</span>
-                            </div>
-                            <div class="blog-text">
-                                <h4>
-                                    <a href="#">Post my imagine Items</a>
-                                </h4>
-                                <p>
-                                    Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit.
-                                </p>
-                            </div>
-                            <span>
+        </div>
+        <div class="blog-text">
+            <h4>
+                <a href="#">Post my imagine Items</a>
+            </h4>
+            <p>
+                Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec
+                sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit.
+            </p>
+        </div>
+        <span>
 										<a href="blog-details.blade.php" class="ready-btn">Read more</a>
 									</span>
-                        </div>
-                    </div>
-                    <!-- Start single blog -->
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <div class="single-blog">
-                            <div class="single-blog-img">
-                                <a href="blog-details.blade.php">
-                                    <img src="img/blog/3.jpg" alt="">
-                                </a>
-                            </div>
-                            <div class="blog-meta">
+    </div>
+</div>
+<!-- Start single blog -->
+<div class="col-md-12 col-sm-12 col-xs-12">
+    <div class="single-blog">
+        <div class="single-blog-img">
+            <a href="blog-details.blade.php">
+                <img src="img/blog/3.jpg" alt="">
+            </a>
+        </div>
+        <div class="blog-meta">
                   <span class="comments-type">
 											<i class="fa fa-comment-o"></i>
 											<a href="#">13 comments</a>
 										</span>
-                                <span class="date-type">
+            <span class="date-type">
 											<i class="fa fa-calendar"></i>2016-03-05 / 09:10:16
 										</span>
-                            </div>
-                            <div class="blog-text">
-                                <h4>
-                                    <a href="#">Post my imagine Items</a>
-                                </h4>
-                                <p>
-                                    Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit.
-                                </p>
-                            </div>
-                            <span>
+        </div>
+        <div class="blog-text">
+            <h4>
+                <a href="#">Post my imagine Items</a>
+            </h4>
+            <p>
+                Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec
+                sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit.
+            </p>
+        </div>
+        <span>
 										<a href="blog-details.blade.php" class="ready-btn">Read more</a>
 									</span>
-                        </div>
-                    </div>
-                    <!-- End single blog -->
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <div class="single-blog">
-                            <div class="single-blog-img">
-                                <a href="blog-details.blade.php">
-                                    <img src="img/blog/4.jpg" alt="">
-                                </a>
-                            </div>
-                            <div class="blog-meta">
+    </div>
+</div>
+<!-- End single blog -->
+<div class="col-md-12 col-sm-12 col-xs-12">
+    <div class="single-blog">
+        <div class="single-blog-img">
+            <a href="blog-details.blade.php">
+                <img src="img/blog/4.jpg" alt="">
+            </a>
+        </div>
+        <div class="blog-meta">
                   <span class="comments-type">
 											<i class="fa fa-comment-o"></i>
 											<a href="#">1 comments</a>
 										</span>
-                                <span class="date-type">
+            <span class="date-type">
 											<i class="fa fa-calendar"></i>2016-03-05 / 09:10:16
 										</span>
-                            </div>
-                            <div class="blog-text">
-                                <h4>
-                                    <a href="#">Post my imagine Items</a>
-                                </h4>
-                                <p>
-                                    Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit.
-                                </p>
-                            </div>
-                            <span>
+        </div>
+        <div class="blog-text">
+            <h4>
+                <a href="#">Post my imagine Items</a>
+            </h4>
+            <p>
+                Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec
+                sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit.
+            </p>
+        </div>
+        <span>
 										<a href="blog-details.blade.php" class="ready-btn">Read more</a>
 									</span>
-                        </div>
-                    </div>
-                    <!-- Start single blog -->
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <div class="single-blog">
-                            <div class="single-blog-img">
-                                <a href="blog-details.blade.php">
-                                    <img src="img/blog/5.jpg" alt="">
-                                </a>
-                            </div>
-                            <div class="blog-meta">
+    </div>
+</div>
+<!-- Start single blog -->
+<div class="col-md-12 col-sm-12 col-xs-12">
+    <div class="single-blog">
+        <div class="single-blog-img">
+            <a href="blog-details.blade.php">
+                <img src="img/blog/5.jpg" alt="">
+            </a>
+        </div>
+        <div class="blog-meta">
                   <span class="comments-type">
 											<i class="fa fa-comment-o"></i>
 											<a href="#">10 comments</a>
 										</span>
-                                <span class="date-type">
+            <span class="date-type">
 											<i class="fa fa-calendar"></i>2016-03-05 / 09:10:16
 										</span>
-                            </div>
-                            <div class="blog-text">
-                                <h4>
-                                    <a href="#">Post my imagine Items</a>
-                                </h4>
-                                <p>
-                                    Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit.
-                                </p>
-                            </div>
-                            <span>
+        </div>
+        <div class="blog-text">
+            <h4>
+                <a href="#">Post my imagine Items</a>
+            </h4>
+            <p>
+                Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec
+                sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit.
+            </p>
+        </div>
+        <span>
 										<a href="blog-details.blade.php" class="ready-btn">Read more</a>
 									</span>
-                        </div>
-                    </div>
-                    <!-- End single blog -->
-                    <div class="blog-pagination">
-                        <ul class="pagination">
-                            <li><a href="#">&lt;</a></li>
-                            <li class="active"><a href="#">1</a></li>
-                            <li><a href="#">2</a></li>
-                            <li><a href="#">3</a></li>
-                            <li><a href="#">&gt;</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
-<!-- End Blog Area -->
+<!-- End single blog -->
+<div class="blog-pagination">
+    <ul class="pagination">
+        <li><a href="#">&lt;</a></li>
+        <li class="active"><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">&gt;</a></li>
+    </ul>
+</div>
