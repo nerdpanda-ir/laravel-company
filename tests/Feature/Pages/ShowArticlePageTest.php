e9739466a7fa4ea7d861d4d29a6a2675ffe5b962
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-30 08:53:55 +0200

    [file]{ShowArticlePageTest.php}=> clean test class

--- tests/Feature/Pages/ShowArticlePageTest.php
+++ tests/Feature/Pages/ShowArticlePageTest.php
@@ -6,15 +6,5 @@ use Tests\TestCase;
 
 class ShowArticlePageTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
+    
 }
