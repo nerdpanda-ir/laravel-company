551db088802b264e095b1e3d5099b12396ea9ee4
NerdPanda <nerdpandabusiness@gmail.com>
2022-05-31 11:42:04 +0200

    [file]{HasInvokeJsonResourceableContract.php}=> define __invoke method !!!

--- app/Contracts/HasInvokeJsonResourceableContract.php
+++ app/Contracts/HasInvokeJsonResourceableContract.php
@@ -1,5 +1,8 @@
 <?php namespace App\Contracts; ?>
 <?php
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
 interface HasInvokeJsonResourceableContract {
-    
+    function __invoke():JsonResource;
 }
