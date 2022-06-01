354f167bd6e6b538223f185842c359d2e584dc63
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-01 06:29:39 +0200

    [file]{ShowArticlePageApiController.php}=> invoke method refactor

--- app/Http/Controllers/Api/V1/ShowArticlePageApiController.php
+++ app/Http/Controllers/Api/V1/ShowArticlePageApiController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Contracts\ShowArticlePageApiControllerContract;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\V1\Pages\ShowArticlePageResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ShowArticlePageApiController extends Controller implements ShowArticlePageApiControllerContract
 {
     public function __invoke(): JsonResource {
-
+        $data = ['message'=>'article details'];
+        return ShowArticlePageResource::make($data);
     }
 }
