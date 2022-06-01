0715ee8f6b0df64bf28e82828406a258073b59f1
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-01 06:33:59 +0200

    [file]{ShowArticlePageApiTest.php}=> create show article page api test

---
+++ tests/Feature/Pages/Api/V1/ShowArticlePageApiTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Pages\Api\V1;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ShowArticlePageApiTest extends TestCase
+{
+    protected string $pageRoute = 'api.v1.page.article.show';
+
+    public function test_home_api_page_should_have_200_status_code():void {
+        $response = $this->get(route($this->pageRoute));
+        $response->assertOk();
+    }
+    public function test_has_contentType_header():void {
+        $url = route($this->pageRoute);
+        $response = $this->get($url);
+        $header = 'content-type';
+        $isExist = $response->headers->has($header);
+        $this->assertTrue($isExist,"get response for $url should have $header header !!!");
+    }
+    public function test_content_type_header_in_home_api_page_response_should_is_application_json():void {
+        $url = route($this->pageRoute);
+        $response = $this->get($url);
+        $header = 'content-type';
+        if (!$response->headers->has($header))
+            $this->fail("no found $header header in " . $url . ' response !!!');
+        $contentType = $response->headers->get($header);
+        $expect = 'application/json';
+        $this->assertTrue(
+            str_contains($contentType, $expect),
+            "in $header header value for $url url should exist  $expect but is $contentType"
+        );
+    }
+
+    public function test_home_api_page_json_response_body():void{
+        $expected = ['data'=> ['message'=> 'article details']];
+        $response = $this->get(route($this->pageRoute));
+        $response->assertJson($expected);
+    }
+}
