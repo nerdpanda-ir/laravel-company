e4e9f64825c1b112593a8d204f92fee810e458df
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-02 12:02:52 +0200

    [file]{BlogPageApiControllerTest.php}=> modify interface namespace

--- app/Http/Controllers/Api/V1/BlogPageApiController.php
+++ app/Http/Controllers/Api/V1/BlogPageApiController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Contracts\BlogPageApiControllerInterface;
+use App\Contracts\BlogPageApiControllerContract;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\Pages\BlogPageResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class BlogPageApiController extends Controller implements BlogPageApiControllerInterface
+class BlogPageApiController extends Controller implements BlogPageApiControllerContract
 {
     public function __invoke():JsonResource {
         $data = [
