028a079534cd116033a9d92247af81ef7fb9d170
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-02 12:28:33 +0200

    [file]{HomePageApiController.php}=> rename interface

--- app/Http/Controllers/Api/V1/HomePageApiController.php
+++ app/Http/Controllers/Api/V1/HomePageApiController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Contracts\HomePageApiControllerInterface;
+use App\Contracts\HomePageApiControllerContract;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\Api\V1\Pages\HomePageResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class HomePageApiController extends Controller implements HomePageApiControllerInterface
+class HomePageApiController extends Controller implements HomePageApiControllerContract
 {
     public function __invoke() :JsonResource {
         $data = [
