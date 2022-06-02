f2d5b0afe659d1fa3f65b3d1d7fb558335023ec0
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-02 12:37:21 +0200

    [file]{ShowArticleControllerInterface.php}=> rename to ShowArticleControllerContract.php

---
+++ app/Contracts/ShowArticleControllerContract.php
@@ -0,0 +1,5 @@
+<?php namespace App\Contracts ; ?>
+<?php
+interface ShowArticleControllerContract extends HasInvokeViewableContract {
+
+}
--- app/Contracts/ShowArticleControllerInterface.php
+++
@@ -1,5 +0,0 @@
-<?php namespace App\Contracts ; ?>
-<?php
-interface ShowArticleControllerInterface extends HasInvokeViewableContract {
-
-}
