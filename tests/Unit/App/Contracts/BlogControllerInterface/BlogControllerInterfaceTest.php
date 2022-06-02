15803791b56ba3c73ea33ab6ad51a81c8f7f5cc3
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-02 10:13:14 +0200

    [file]{BlogControllerInterfaceTest.php}=> add test method , rename test method , refactor test method !!!

--- tests/Unit/App/Contracts/BlogControllerInterface/BlogControllerInterfaceTest.php
+++ tests/Unit/App/Contracts/BlogControllerInterface/BlogControllerInterfaceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\App\Contracts\BlogControllerInterface;
 
+use App\Contracts\HasInvokeViewableContract;
 use PHPUnit\Framework\TestCase;
 use App\Contracts\BlogControllerInterface;
 
@@ -12,8 +13,28 @@ class BlogControllerInterfaceTest extends TestCase
         $isExist = interface_exists($this->namespace);
         $this->assertTrue($isExist,"missing $this->namespace interface !!");
     }
-    public function test_no_extend_interface():void {
-        $extends = class_implements($this->namespace);
-        $this->assertEmpty($extends,"$this->namespace interface should is never extend any interface ");
+    public function test_should_extend_interface():void {
+        $expect = 1 ;
+        $classImplements = class_implements($this->namespace);
+        $classImplementsCount = count($classImplements);
+        $message = '';
+        if ($classImplementsCount!=$expect){
+            $message = "interface $this->namespace implement ";
+            $interfaceImplode = '';
+            if ($classImplementsCount>=1)
+                $interfaceImplode ='-> '.implode(' , ',$classImplements);
+            if ($classImplementsCount>$expect)
+                $message.='more';
+            else
+                $message.='less';
+            $message.=" than expect interfaces !!!\nshould implement $expect interface but implement $classImplementsCount interface $interfaceImplode";
+        }
+        $this->assertEquals($classImplementsCount,$expect,$message);
+    }
+    public function test_should_extend_HasInvokeJsonResourceableContract_interface():void {
+        $classImplements = class_implements($this->namespace);
+        $interface = HasInvokeViewableContract::class ;
+        $isImplement = in_array($interface, $classImplements);
+        $this->assertTrue($isImplement,"$this->namespace interface should implement $interface !!!");
     }
 }
