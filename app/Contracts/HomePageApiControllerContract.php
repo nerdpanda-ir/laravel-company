c1c4a2b450a0224aedc2daaaf45e56ad0532e2f1
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-02 12:23:01 +0200

    [file]{HomePageApiControllerInterface.php}=>rename to HomePageApiControllerContract.php

---
+++ app/Contracts/HomePageApiControllerContract.php
@@ -0,0 +1,5 @@
+<?php namespace App\Contracts; ?>
+<?php
+interface HomePageApiControllerContract extends HasInvokeJsonResourceableContract {
+
+}
--- app/Contracts/HomePageApiControllerInterface.php
+++
@@ -1,5 +0,0 @@
-<?php namespace App\Contracts; ?>
-<?php
-interface HomePageApiControllerInterface extends HasInvokeJsonResourceableContract {
-
-}
