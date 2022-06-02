2ad0f70c250afa8e5fe61a6938b2d947987cd19b
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-02 10:19:04 +0200

    [file]{BlogControllerTest.php}=> test method refactor

--- tests/Unit/App/Http/Controllers/BlogController/BlogControllerTest.php
+++ tests/Unit/App/Http/Controllers/BlogController/BlogControllerTest.php
@@ -36,8 +36,20 @@ class BlogControllerTest extends TestCase
         $selfImplements = class_implements($this->namespace);
         $diff = array_diff($selfImplements,$parentImplements);
         $diffCount = count($diff);
-        $expect = 1 ;
-        $this->assertEquals($expect,$diffCount,"controller $this->namespace should implement $expect interface but implement $diffCount interface !! ");
+        $expect = 2 ;
+        $message = '' ;
+        if ($expect!=$diffCount){
+            $message .= "$this->namespace controller ";
+            $implodeInterfaces = '';
+            if ($diffCount>0)
+                $implodeInterfaces = ' -> '.implode(' , ',$diff);
+            if ($diffCount>$expect)
+                $message .= 'more';
+            else
+                $message .= 'less';
+            $message.=" than expect implement interface !!!\n$this->namespace should implement $expect interface but implement $diffCount interface $implodeInterfaces !!!";
+        }
+        $this->assertEquals($expect,$diffCount,$message);
     }
     public function test_should_implement_BlogControllerInterface():void {
         $parent = get_parent_class($this->namespace);
@@ -53,12 +65,4 @@ class BlogControllerTest extends TestCase
         $uses = class_uses($this->namespace);
         $this->assertEmpty($uses," controller $this->namespace dont be use trait !!!");
     }
-
-    /*public function test_invoke_method_should_return_instance_of_View() :void {
-       $this->assertInvokeMethodReturnViewInstance();
-   }
-
-   public function test_invoke_method_should_return_blog_view():void {
-       $this->assertReturnedViewForInvokeMethodShouldIs('blog');
-   }*/
 }
