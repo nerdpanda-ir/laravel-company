3cfb9a858d5c6d0fcd9212826193567147134b7c
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-04 07:33:26 +0200

    [file]{ArticleApiController.php}=> implement interface

--- app/Http/Controllers/Api/V1/ArticleApiController.php
+++ app/Http/Controllers/Api/V1/ArticleApiController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Contracts\ArticleApiControllerContract;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ArticleApiController extends Controller
+class ArticleApiController extends Controller implements ArticleApiControllerContract
 {
     //
 }
