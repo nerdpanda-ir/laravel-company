856399bfc8e6e909e501e9390c68b24da28e8e96
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-04 07:37:50 +0200

    [file]{ArticleApiController.php}=> implement index method

--- app/Http/Controllers/Api/V1/ArticleApiController.php
+++ app/Http/Controllers/Api/V1/ArticleApiController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Contracts\ArticleApiControllerContract;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleResourceCollection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class ArticleApiController extends Controller implements ArticleApiControllerContract
 {
-    //
+    public function index(): JsonResource {
+        $data = ['message'=>'all articles'];
+        return ArticleResourceCollection::make($data);
+    }
 }
