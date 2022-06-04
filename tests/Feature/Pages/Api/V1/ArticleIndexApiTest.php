8839956f56c4ea4714e381038a0a9bde9003d947
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-04 09:45:36 +0200

    [file]{ArticleIndexApiTest.php}=> create test class

---
+++ tests/Feature/Pages/Api/V1/ArticleIndexApiTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Pages\Api\V1;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ArticleIndexApiTest extends TestCase
+{
+    protected string $pageRoute = 'api.v1.article.index';
+
+    public function test_should_have_200_status_code():void {
+        $response = $this->get(route($this->pageRoute));
+        $response->assertOk();
+    }
+    public function test_has_contentType_header():void {
+        $url = route($this->pageRoute);
+        $response = $this->get($url);
+        $header = 'content-type';
+        $isExist = $response->headers->has($header);
+        $this->assertTrue($isExist,"get response for $url should have $header header !!!");
+    }
+    public function test_content_type_header_in_response_should_is_application_json():void {
+        $url = route($this->pageRoute);
+        $response = $this->get($url);
+        $header = 'content-type';
+        if (!$response->headers->has($header))
+            $this->fail("no found $header header in " . $url . ' response !!!');
+        $contentType = $response->headers->get($header);
+        $expect = 'application/json';
+        $this->assertTrue(
+            str_contains($contentType, $expect),
+            "in $header header value for $url url should exist  $expect but is $contentType"
+        );
+    }
+
+    public function test_json_response_body():void{
+        $expected = ['data'=> ['message'=> 'all articles']];
+        $response = $this->get(route($this->pageRoute));
+        $response->assertJson($expected);
+    }
+}
