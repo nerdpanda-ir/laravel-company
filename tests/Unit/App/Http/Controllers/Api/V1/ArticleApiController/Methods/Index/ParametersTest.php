c21d469e94c801d801370d8e35472e7dee0874d4
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-04 08:18:18 +0200

    [file]{ParametersTest.php}=> modify property value

--- tests/Unit/App/Http/Controllers/Api/V1/ArticleApiController/Methods/Index/ParametersTest.php
+++ tests/Unit/App/Http/Controllers/Api/V1/ArticleApiController/Methods/Index/ParametersTest.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Api\V1\ArticleApiController as Entity;
 
 class ParametersTest extends TestCase
 {
-    protected string $namespace = ShowArticlePageApiController::class ;
+    protected string $namespace = Entity::class ;
     protected string $method = 'index';
     public function test_no_have_parameter():void{
         $reflection = new \ReflectionMethod($this->namespace , $this->method);
