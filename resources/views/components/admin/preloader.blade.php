4db1021284977aababb1613dcb5acf2406434b9c
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-08 08:28:42 +0200

    [file]{preloader.blade.php}=> add preloader codes !!!

---
+++ resources/views/components/admin/preloader.blade.php
@@ -0,0 +1,6 @@
+@production
+<!-- Preloader -->
+<div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__wobble" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+</div>
+@endproduction
