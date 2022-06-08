8933226f891fe7131acb4ce805599f1e0c581c96
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-08 11:59:01 +0200

    [file]{index.blade.php}=> remove footer codes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,13 +4,6 @@
     <x-admin.wrapper>
         <x-partials.admin.header />
         <x-partials.pages.admin.dashboard.main />
-        <!-- Main Footer -->
-        <footer class="main-footer">
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-            All rights reserved.
-            <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.2.0
-            </div>
-        </footer>
+
     </x-admin.wrapper>
 @endsection
