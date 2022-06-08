b04c019076d983b4462171562bcb057307191caa
NerdPanda <nerdpandabusiness@gmail.com>
2022-06-08 08:28:22 +0200

    [file]{index.blade.php}=> delete preloader codes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,10 +2,7 @@
 @section('title') AdminLTE 3 | Dashboard 2 @endsection
 @section('main')
     <x-admin.wrapper>
-        <!-- Preloader -->
-        <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__wobble" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-        </div>
+
 
         <!-- Navbar -->
         <nav class="main-header navbar navbar-expand navbar-dark">
